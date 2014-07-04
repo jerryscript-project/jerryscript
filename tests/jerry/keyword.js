e4122522aeb765507458fa0f4015af538d20ab7e
e.gavrin <e.gavrin@samsung.com>
2014-07-04 14:20:06 +0400

    tests

---
+++ tests/jerry/keyword.js
@@ -0,0 +1,26 @@
+// Copyright 2014 Samsung Electronics Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// tokenize
+break do instanceof typeof
+case else new var
+catch finally return void
+continue for switch while
+debugger function this with
+default if throw
+delete in try
+class enum extends super
+const export import
+implements let private public yield
+interface package protected static

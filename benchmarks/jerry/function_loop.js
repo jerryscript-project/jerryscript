123df0f18511854130304eda1d46b6a47cb57a44
e.gavrin <e.gavrin@samsung.com>
2014-07-21 15:01:55 +0400

    added benchmarks.js

---
+++ benchmarks/jerry/function_loop.js
@@ -0,0 +1,35 @@
+// Copyright 2014 Samsung Electronics Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var x = 7;
+var y = 3;
+
+function cse_opt(x, y)
+{
+    var tmp1 = x * x;
+    var tmp2 = y * y;
+    var tmp3 = tmp1 * tmp1;
+    var tmp4 = tmp2 * tmp2;
+    
+    for (var i = 0; i < count; i++) {
+        var cached1 = tmp3 * x
+        var cached2 = tmp4 * y;
+        var cached_n_cached = cached1 + cached2;
+        var ret_val = cached_n_cached + cached_n_cached;
+    }
+    
+    return ret_val + ret_val;
+};
+
+cse_opt(x, y);

cd0246bedcd9320682140193e0bde428c8bbdb65
Ruben Ayrapetyan <r.ayrapetyan@samsung.com>
2014-08-08 18:23:02 +0400

    Changing size of ecma-array's chunk to fixed 8 bytes.

--- src/libecmaobjects/ecma-alloc.c
+++ src/libecmaobjects/ecma-alloc.c
@@ -45,8 +45,8 @@ FIXME( Pack ecma_object_t )
 JERRY_STATIC_ASSERT( sizeof (ecma_object_t) <= 2 * sizeof (uint64_t) );
 
 JERRY_STATIC_ASSERT( sizeof (ecma_array_header_t) <= sizeof (uint32_t) );
-JERRY_STATIC_ASSERT( sizeof (ecma_array_first_chunk_t) == ECMA_ARRAY_CHUNK_SIZE_IN_BYTES );
-JERRY_STATIC_ASSERT( sizeof (ecma_array_non_first_chunk_t) == ECMA_ARRAY_CHUNK_SIZE_IN_BYTES );
+JERRY_STATIC_ASSERT( sizeof (ecma_array_first_chunk_t) == sizeof(uint64_t) );
+JERRY_STATIC_ASSERT( sizeof (ecma_array_non_first_chunk_t) == sizeof(uint64_t) );
 JERRY_STATIC_ASSERT( sizeof (ecma_completion_value_t) == sizeof(uint32_t) );
 
 /**

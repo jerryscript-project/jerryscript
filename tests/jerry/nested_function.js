4fb008070164274e2adf65060155bac4fa3859e6
Ruben Ayrapetyan <r.ayrapetyan@samsung.com>
2014-08-08 23:20:34 +0400

    Adding test nested_function.js.

---
+++ tests/jerry/nested_function.js
@@ -0,0 +1,40 @@
+// Copyright 2014 Samsung Electronics Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var k;
+
+function f1()
+{
+  k = 17;
+
+  f2();
+
+  function f2()
+    {
+      assert(k > 0);
+      assert(i < 10000);
+
+      if(--k == 0)
+        {
+          return;
+        }
+
+      f2();
+    }
+}
+
+for(var i = 0; i < 10000; i++)
+{
+  f1();
+}

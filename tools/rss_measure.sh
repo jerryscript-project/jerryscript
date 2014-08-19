abd686242a9a04c5c9c09b8d4f4d3aa2c4d2e4b4
e.gavrin <e.gavrin@samsung.com>
2014-08-19 13:31:40 +0400

    Reorder fields in rss_measure.sh

--- tools/rss_measure.sh
+++ tools/rss_measure.sh
@@ -20,14 +20,15 @@ RAW_OUTPUT=$3
 SLEEP=0.3
 REPEATS=5
 
-Size_OUT=""
+
 Rss_OUT=""
 Pss_OUT=""
-Share_OUT=""
-Shared_Clean_OUT=""
-Shared_Dirty_OUT=""
-Private_Clean_OUT=""
 Private_Dirty_OUT=""
+Private_Clean_OUT=""
+Shared_Dirty_OUT=""
+Shared_Clean_OUT=""
+Share_OUT=""
+Size_OUT=""
 Swap_OUT=""
 
 function collect_entry()
@@ -35,11 +36,11 @@ function collect_entry()
   OUT_NAME="$1_OUT";
   OUT=$OUT_NAME;
 
-  SUM=`cat /proc/$PID/smaps 2>/dev/null | grep $1 | awk '{sum += $2;} END { if (sum != 0) { print sum; }; }'`;
+  SUM=$(grep "$1" < /proc/"$PID"/smaps | awk '{sum += $2;} END { if (sum != 0) { print sum; }; }');
   
   if [ "$SUM" != "" ];
   then
-    eval "$OUT"="\"\$$OUT $SUM\\n\"";
+    eval "$OUT=\"\$$OUT $SUM\\n\"";
   fi;
 }
 
@@ -58,14 +59,14 @@ function run_test()
 
   while kill -0 "$PID" > /dev/null 2>&1;
   do
-    collect_entry Size
     collect_entry Rss
     collect_entry Pss
-    collect_entry Share
-    collect_entry Shared_Clean
-    collect_entry Shared_Dirty
-    collect_entry Private_Clean
     collect_entry Private_Dirty
+    collect_entry Private_Clean
+    collect_entry Shared_Dirty
+    collect_entry Shared_Clean
+    collect_entry Share
+    collect_entry Size
     collect_entry Swap
     
     sleep $SLEEP
@@ -88,39 +89,39 @@ echo
 
 if [ "$RAW_OUTPUT" != "" ];
 then
-   echo -e "$Size_OUT";
    echo -e "$Rss_OUT";
    echo -e "$Pss_OUT";
-   echo -e "$Share_OUT";
-   echo -e "$Shared_Clean_OUT";
-   echo -e "$Shared_Dirty_OUT";
-   echo -e "$Private_Clean_OUT";
    echo -e "$Private_Dirty_OUT";
+   echo -e "$Private_Clean_OUT";
+   echo -e "$Shared_Dirty_OUT";
+   echo -e "$Shared_Clean_OUT";
+   echo -e "$Share_OUT";
+   echo -e "$Size_OUT";
    echo -e "$Swap_OUT";
 fi;
 
 if [ "$Size_OUT" == "" ]
 then
-  echo ===================
+  echo "==================="
   echo "Test failed."
-  echo ===================
+  echo "==================="
   exit 1
 fi;
 
 TIME=$(echo "scale=3;($FINISH - $START) / 1.0" | bc );
 AVG_TIME=$(echo "scale=3;$TIME / $REPEATS" | bc );
 
-echo ===================
+echo "==================="
 printf "%24sAVERAGE%28sMAX\n" "" "";
-print_entry Size
 print_entry Rss
 print_entry Pss
-print_entry Share
-print_entry Shared_Clean
-print_entry Shared_Dirty
-print_entry Private_Clean
 print_entry Private_Dirty
+print_entry Private_Clean
+print_entry Shared_Dirty
+print_entry Shared_Clean
+print_entry Share
+print_entry Size
 print_entry Swap
 echo -e "---"
 echo -e "Exec time / average:\t$TIME / $AVG_TIME secs"
-echo ===================
+echo "==================="

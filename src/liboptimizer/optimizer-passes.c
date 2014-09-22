615903dbf5b67cc590a51acc28caa6bc867439e3
Ilmir Usmanov <i.usmanov@samsung.com>
2014-09-22 19:58:08 +0400

    Fix call expression without args bytecode generation

--- src/liboptimizer/optimizer-passes.c
+++ src/liboptimizer/optimizer-passes.c
@@ -21,39 +21,8 @@
 
 #define NAME_TO_ID(op) (__op__idx_##op)
 
-/* Reorder bytecode like
-
-   call_n ...
-   assignment ... +
-   var_?_end
-
-   to
-
-   assignment ... +
-   call_n ...
-   var_?_end
- */
-static void
-optimize_calls (opcode_t *opcodes)
-{
-  opcode_t *current_opcode;
-
-  for (current_opcode = opcodes;
-       current_opcode->op_idx != NAME_TO_ID (exitval);
-       current_opcode++)
-  {
-    if (current_opcode->op_idx == NAME_TO_ID (call_n)
-        && (current_opcode + 1)->op_idx == NAME_TO_ID (assignment))
-    {
-      opcode_t temp = *current_opcode;
-      *current_opcode = *(current_opcode + 1);
-      *(current_opcode + 1) = temp;
-    }
-  }
-}
-
 void
-optimizer_run_passes (opcode_t *opcodes)
+optimizer_run_passes (opcode_t *opcodes __unused)
 {
-  optimize_calls (opcodes);
+  FIXME (/*Write optimizer when postparser will be ready.  */)
 }

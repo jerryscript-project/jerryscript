ba4dca2e9894087d760a608ea7269cb71a095d7d
Ruben Ayrapetyan <r.ayrapetyan@samsung.com>
2014-10-20 14:15:01 +0400

    Fixing some tests from internal test suite.

--- tests/jerry-test-suite/15/15.08/15.08.02/15.08.02.01/15.08.02.01-004.js
+++ tests/jerry-test-suite/15/15.08/15.08.02/15.08.02.01/15.08.02.01-004.js
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-asseert(Math.abs(Number.NEGATIVE_INFINITY) === Number.POSITIVE_INFINITY);
+assert(Math.abs(Number.NEGATIVE_INFINITY) === Number.POSITIVE_INFINITY);
--- tests/jerry-test-suite/15/15.08/15.08.02/15.08.02.11/15.08.02.11-004.js
+++ tests/jerry-test-suite/15/15.08/15.08.02/15.08.02.11/15.08.02.11-004.js
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-aseert(isNaN(Math.max(NaN)));
+assert(isNaN(Math.max(NaN)));

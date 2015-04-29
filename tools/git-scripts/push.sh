19aea7a502e90851f95380dfaaf28887e504dd43
Ruben Ayrapetyan <r.ayrapetyan@samsung.com>
2015-04-29 14:54:17 +0300

    Fixing bash scripts: moving '#!/bin/bash' to top.
    JerryScript-DCO-1.0-Signed-off-by: Ruben Ayrapetyan r.ayrapetyan@samsung.com

--- tools/git-scripts/push.sh
+++ tools/git-scripts/push.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Copyright 2014-2015 Samsung Electronics Co., Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#!/bin/bash
 
 GIT_STATUS_NOT_CLEAN_MSG="Git status of current directory is not clean"
 GIT_STATUS_CONSIDER_CLEAN_MSG="Consider removing all untracked files, locally commiting all changes and running $0 again"
--- tools/perf.sh
+++ tools/perf.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Copyright 2014 Samsung Electronics Co., Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#!/bin/bash
 
 ITERS=$1
 ENGINE=$2

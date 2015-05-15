6edf6d3a8182362f228b25d5932b579632e29aed
László Langó <llango.u-szeged@partner.samsung.com>
2015-05-15 10:54:03 +0200

    Continue RegExp parser implementation.
    Add parsing of some general token types.
    
    JerryScript-DCO-1.0-Signed-off-by: László Langó llango.u-szeged@partner.samsung.com

--- jerry-core/parser/regexp/re-parser.cpp
+++ jerry-core/parser/regexp/re-parser.cpp
@@ -16,11 +16,82 @@
 
 #include "re-parser.h"
 
+#define JERRY_LOOKUP(str_p, lookup)  *(str_p + lookup)
+
 operand
 parse_regexp_literal ()
 {
   // FIXME: Impelement this!
   return operand ();
+}
+
+static uint32_t
+parse_re_iterator (ecma_char_t **pattern,
+                   re_token_t *re_token,
+                   uint32_t lookup)
+{
+  uint32_t advance = 0;
+  re_token->qmin = 1;
+  re_token->qmax = 1;
+  re_token->greedy = true;
+
+  ecma_char_t ch0 = JERRY_LOOKUP (*pattern, lookup);
+  ecma_char_t ch1 = JERRY_LOOKUP (*pattern, lookup + 1);
+
+  switch (ch0)
+  {
+    case '?':
+      re_token->qmin = 0;
+      re_token->qmax = 1;
+      if (ch1 == '?')
+      {
+        advance = 2;
+        re_token->greedy = false;
+      }
+      else
+      {
+        advance = 1;
+        re_token->greedy = true;
+      }
+      break;
+    case '*':
+      re_token->qmin = 1;
+      re_token->qmax = RE_ITERATOR_INFINITE;
+      if (ch1 == '?')
+      {
+        advance = 2;
+        re_token->greedy = false;
+      }
+      else
+      {
+        advance = 1;
+        re_token->greedy = true;
+      }
+      break;
+    case '+':
+      re_token->qmin = 1;
+      re_token->qmax = RE_ITERATOR_INFINITE;
+      if (ch1 == '?')
+      {
+        advance = 2;
+        re_token->greedy = false;
+      }
+      else
+      {
+        advance = 1;
+        re_token->greedy = true;
+      }
+      break;
+    case '{':
+    {
+      JERRY_UNREACHABLE ();
+      break;
+    }
+  }
+
+  /* FIXME: Throw a syntax error if (qmin > qmax) */
+
+  return advance;
 }
 
 /**
@@ -30,19 +101,77 @@ re_token_t
 re_parse_next_token (ecma_char_t **pattern)
 {
   re_token_t result;
-
+  uint32_t advance = 0;
   ecma_char_t ch = **pattern;
 
   switch (ch)
   {
+    case '|':
+      advance = 1;
+      result.type = RE_TOK_ALTERNATIVE;
+      break;
+    case '^':
+      advance = 1;
+      result.type = RE_TOK_ASSERT_START;
+      break;
+    case '$':
+      advance = 1;
+      result.type = RE_TOK_ASSERT_END;
+      break;
+    case '.':
+      advance = 1;
+      advance += parse_re_iterator (pattern, &result, 1);
+      result.type = RE_TOK_ATOM_PERIOD;
+      break;
+    case '(':
+      if (JERRY_LOOKUP (*pattern, 1) == '?') {
+          if (JERRY_LOOKUP (*pattern, 2) == '=') {
+            /* (?= */
+            advance = 3;
+            result.type = RE_TOK_ASSERT_START_POS_LOOKAHEAD;
+          } else if (JERRY_LOOKUP (*pattern, 2) == '!') {
+            /* (?! */
+            advance = 3;
+            result.type = RE_TOK_ASSERT_START_NEG_LOOKAHEAD;
+          } else if (JERRY_LOOKUP (*pattern, 2) == ':') {
+            /* (?: */
+            advance = 3;
+            result.type = RE_TOK_ATOM_START_NONCAPTURE_GROUP;
+          }
+        } else {
+          /* ( */
+          advance = 1;
+          result.type = RE_TOK_ATOM_START_CAPTURE_GROUP;
+        }
+        break;
+    case ')':
+      advance = 1;
+      result.type = RE_TOK_ATOM_START_CAPTURE_GROUP;
+    case '[':
+    case ']':
+    case '}':
+    case '\\':
+    {
+      JERRY_UNIMPLEMENTED ("Unimplemented RegExp token!");
+      break;
+    }
+    case '?':
+    case '*':
+    case '+':
+    case '{':
+      JERRY_UNREACHABLE ();
+      break;
     default:
     {
+      advance = 1;
+      advance += parse_re_iterator (pattern, &result, 1);
       result.type = RE_TOK_CHAR;
       result.value = ch;
     }
   }
 
-  (*pattern)++;
+  JERRY_ASSERT (advance > 0);
+  (*pattern) += advance;
 
   return result;
 } /* re_parse_next_token */

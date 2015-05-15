eac7372f41127928b1a6f7c2eb75a7430e684788
Peter Gal <pgal.u-szeged@partner.samsung.com>
2015-05-15 15:57:26 +0200

    Improve compiler detection for x86_64 target
    JerryScript-DCO-1.0-Signed-off-by: Peter Gal pgal.u-szeged@partner.samsung.com

--- build/configs/toolchain_linux_x86_64.cmake
+++ build/configs/toolchain_linux_x86_64.cmake
@@ -15,7 +15,9 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-set(CMAKE_C_COMPILER x86_64-linux-gnu-gcc)
-set(CMAKE_CXX_COMPILER x86_64-linux-gnu-g++)
+find_program(CMAKE_C_COMPILER NAMES x86_64-linux-gnu-gcc x86_64-unknown-linux-gnu-gcc)
+find_program(CMAKE_CXX_COMPILER NAMES x86_64-linux-gnu-g++ x86_64-unknown-linux-gnu-g++)
+# FIXME: This could break cross compilation, when the strip is not for the target architecture
+find_program(CMAKE_STRIP NAMES x86_64-linux-gnu-strip x86_64-unknown-linux-gnu-strip strip)
 
 set(FLAGS_COMMON_ARCH -ffixed-rbp)

a80282a082be2f41e7c01be8d5ce2f4fc3c9c536
Dániel Bátyai <dbatyai.u-szeged@partner.samsung.com>
2015-05-21 13:11:24 +0200

    Implemented Array.prototype.filter()
    JerryScript-DCO-1.0-Signed-off-by: Dániel Bátyai dbatyai.u-szeged@partner.samsung.com

--- jerry-core/ecma/builtin-objects/ecma-builtin-array-prototype.inc.h
+++ jerry-core/ecma/builtin-objects/ecma-builtin-array-prototype.inc.h
@@ -74,6 +74,7 @@ ROUTINE (ECMA_MAGIC_STRING_EVERY, ecma_builtin_array_prototype_object_every, 2, 
 ROUTINE (ECMA_MAGIC_STRING_SOME, ecma_builtin_array_prototype_object_some, 2, 1)
 ROUTINE (ECMA_MAGIC_STRING_SLICE, ecma_builtin_array_prototype_object_slice, 2, 2)
 ROUTINE (ECMA_MAGIC_STRING_SPLICE, ecma_builtin_array_prototype_object_splice, NON_FIXED, 2)
+ROUTINE (ECMA_MAGIC_STRING_FILTER, ecma_builtin_array_prototype_object_filter, 2, 1)
 
 #undef OBJECT_ID
 #undef SIMPLE_VALUE

0ef0b0d59595d67235aee266a169d6925053e0d4
Dániel Bátyai <dbatyai.u-szeged@partner.samsung.com>
2015-05-21 15:18:21 +0200

    Implemented Array.prototype.reverse()
    JerryScript-DCO-1.0-Signed-off-by: Dániel Bátyai dbatyai.u-szeged@partner.samsung.com

---
+++ tests/jerry/array_prototype_reverse.js
@@ -0,0 +1,46 @@
+// Copyright 2015 Samsung Electronics Co., Ltd.
+// Copyright 2015 University of Szeged.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var array = [4, 3, 2, 1, 0]
+
+array.reverse();
+
+for (i = 0; i < array.length; i++) {
+	assert(array[i] === i);
+}
+
+// Checking behavior when unable to get length
+var obj = { reverse : Array.prototype.reverse };
+Object.defineProperty(obj, 'length', { 'get' : function () {throw new ReferenceError ("foo"); } });
+
+try {
+	obj.reverse();
+	assert(false);
+} catch (e) {
+	assert(e.message === "foo");
+	assert(e instanceof ReferenceError);
+}
+
+// Checking behavior when unable to get element
+var obj = { reverse : Array.prototype.reverse, length : 3 };
+Object.defineProperty(obj, '0', { 'get' : function () {throw new ReferenceError ("foo"); } });
+
+try {
+	obj.reverse();
+	assert(false);
+} catch (e) {
+	assert(e.message === "foo");
+	assert(e instanceof ReferenceError);
+}

e358778a5fa5b8ce4c48e666670a81997a69a2b8
Dániel Bátyai <dbatyai.u-szeged@partner.samsung.com>
2015-05-21 10:15:05 +0200

    Implemented Array.prototype.every()
    JerryScript-DCO-1.0-Signed-off-by: Dániel Bátyai dbatyai.u-szeged@partner.samsung.com

---
+++ tests/jerry/array_prototype_every.js
@@ -0,0 +1,62 @@
+// Copyright 2015 Samsung Electronics Co., Ltd.
+// Copyright 2015 University of Szeged.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var array = ["foo", [], Infinity, 4];
+
+function f(arg1, arg2, arg3) {
+	assert(arg1 === array[arg2]);
+	assert(arg3 === array);
+	return true;
+}
+
+assert(array.every(f) === true);
+
+function g(arg1, arg2, arg3) {
+	if (arg1 === 1) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+var arr1 = [1, 1, 1, 1, 1, 2];
+assert(arr1.every(g) === false);
+
+var arr2 = [1, 1, 1, 1, 1, 1];
+assert(arr2.every(g) === true);
+
+// Checking behavior when unable to get length
+var obj = { every : Array.prototype.every };
+Object.defineProperty(obj, 'length', { 'get' : function () {throw new ReferenceError ("foo"); } });
+
+try {
+	obj.every(f);
+	assert(false);
+} catch (e) {
+	assert(e.message === "foo");
+	assert(e instanceof ReferenceError);
+}
+
+// Checking behavior when unable to get element
+var obj = { every : Array.prototype.every, length : 1};
+Object.defineProperty(obj, '0', { 'get' : function () {throw new ReferenceError ("foo"); } });
+
+try {
+	obj.every(f);
+	assert(false);
+} catch (e) {
+	assert(e.message === "foo");
+	assert(e instanceof ReferenceError);
+}

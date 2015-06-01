ee4d854625de11d404f975bba26ebc481a684d84
Zsolt Borbély <zsborbely.u-szeged@partner.samsung.com>
2015-06-01 14:19:07 +0200

    Implement String.prototype.slice()
    JerryScript-DCO-1.0-Signed-off-by: Zsolt Borbély zsborbely.u-szeged@partner.samsung.com

---
+++ tests/jerry/string_prototype_slice.js
@@ -0,0 +1,59 @@
+// Copyright 2015 Samsung Electronics Co., Ltd.
+// Copyright 2015 University of Szeged.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var str = "universe";
+var res;
+
+res = str.slice();
+assert (res === "universe");
+
+res = str.slice(1, 6);
+assert (res === "niver");
+
+res = str.slice("a", "-4");
+assert (res === "univ");
+
+res = str.slice(-5);
+assert (res === "verse");
+
+res = str.slice(-12, undefined);
+assert (res === "universe");
+
+res = str.slice(undefined, -20);
+assert (res === "");
+
+res = str.slice(undefined, undefined);
+assert (res === "universe");
+
+res = str.slice(Infinity, NaN);
+assert (res === "");
+
+res = str.slice(-Infinity, Infinity);
+assert (res === "universe");
+
+res = str.slice(NaN, -Infinity);
+assert (res === "");
+
+res = str.slice(false, true);
+assert (res === "u");
+
+var x;
+res = str.slice(x, x);
+assert (res === "universe");
+
+var obj = {y: "foo"};
+var arr = [x, x];
+res = str.slice(obj, arr);
+assert (res === "");

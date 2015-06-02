4c10a6d0570eed37ddf33383a14c946fd8fbaf16
Ruben Ayrapetyan <r.ayrapetyan@samsung.com>
2015-06-02 20:21:24 +0300

    Restoring lost review fixes for pull request #102 (instantiation of Arguments object).
    JerryScript-DCO-1.0-Signed-off-by: Ruben Ayrapetyan r.ayrapetyan@samsung.com

--- tests/jerry/arguments.js
+++ tests/jerry/arguments.js
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+function f_arg (arguments)
+{
+  return arguments;
+}
+assert (f_arg (1) === 1);
+
 function f (a, b, c)
 {
   return arguments;

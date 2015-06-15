a0a09eb0c73ed9ea9ae019db61ff0160cf69f1bf
Laszlo Vidacs <lvidacs.u-szeged@partner.samsung.com>
2015-06-15 15:37:27 +0200

    Implement String.prototype.trim()
    JerryScript-DCO-1.0-Signed-off-by: Laszlo Vidacs lvidacs.u-szeged@partner.samsung.com

--- jerry-core/ecma/builtin-objects/ecma-builtin-string-prototype.cpp
+++ jerry-core/ecma/builtin-objects/ecma-builtin-string-prototype.cpp
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+#include <ctype.h>
 #include "ecma-alloc.h"
 #include "ecma-builtins.h"
 #include "ecma-conversion.h"
@@ -554,7 +555,62 @@ ecma_builtin_string_prototype_object_to_locale_upper_case (ecma_value_t this_arg
 static ecma_completion_value_t
 ecma_builtin_string_prototype_object_trim (ecma_value_t this_arg) /**< this argument */
 {
-  ECMA_BUILTIN_CP_UNIMPLEMENTED (this_arg);
+  ecma_completion_value_t ret_value = ecma_make_empty_completion_value ();
+
+  /* 1 */
+  ECMA_TRY_CATCH (check_coercible_val,
+                  ecma_op_check_object_coercible (this_arg),
+                  ret_value);
+
+  /* 2 */
+  ECMA_TRY_CATCH (to_string_val,
+                  ecma_op_to_string (this_arg),
+                  ret_value);
+
+  ecma_string_t *original_string_p = ecma_get_string_from_value (to_string_val);
+  JERRY_ASSERT (ecma_string_get_length (original_string_p) >= 0);
+
+  /* 3 */
+  const uint32_t len = (uint32_t) ecma_string_get_length (original_string_p);
+
+  uint32_t prefix = 0, postfix = 0;
+  uint32_t new_len = 0;
+
+  while (prefix < len &&
+         ecma_is_completion_value_empty (ret_value) &&
+         isspace (ecma_string_get_char_at_pos (original_string_p, prefix)))
+  {
+    prefix++;
+  }
+
+  while (postfix < len &&
+         ecma_is_completion_value_empty (ret_value) &&
+         isspace (ecma_string_get_char_at_pos (original_string_p, len-postfix-1)))
+  {
+    postfix++;
+  }
+
+  new_len = len - prefix - postfix;
+
+  MEM_DEFINE_LOCAL_ARRAY (new_str_buffer, new_len + 1, ecma_char_t);
+
+  for (uint32_t idx = 0; idx < new_len; ++idx)
+  {
+    new_str_buffer[idx] = ecma_string_get_char_at_pos (original_string_p, idx + prefix);
+  }
+
+  new_str_buffer[new_len] = '\0';
+  ecma_string_t *new_str_p = ecma_new_ecma_string ((ecma_char_t *) new_str_buffer);
+
+  /* 4 */
+  ret_value = ecma_make_normal_completion_value (ecma_make_string_value (new_str_p));
+
+  MEM_FINALIZE_LOCAL_ARRAY (new_str_buffer);
+
+  ECMA_FINALIZE (to_string_val);
+  ECMA_FINALIZE (check_coercible_val);
+
+  return ret_value;
 } /* ecma_builtin_string_prototype_object_trim */
 
 /**

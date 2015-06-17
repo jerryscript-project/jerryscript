5dc44b136288a8991414a1ab851f41a2babd51e4
Peter Gal <pgal.u-szeged@partner.samsung.com>
2015-06-17 13:55:30 +0200

    Fix MCU script transformation logic.
    The old script behaved incorrectly as it replaced all '*' occurrences
    with the list of files in the given directory.
    
    JerryScript-DCO-1.0-Signed-off-by: Peter Gal pgal.u-szeged@partner.samsung.com

--- tools/generator.sh
+++ tools/generator.sh
@@ -15,9 +15,6 @@
 # limitations under the License.
 
 echo "#define JERRY_MCU_SCRIPT \\" > $2
-cat $1 | while read line
-do
-  line=$(echo $line | sed 's/"/\\"/g')
-  echo "\"$line\n\" \\" >> $2
-done
+# escape all " characters, wrap each line in double quotes and end the line with '\'
+sed 's/"/\\"/g' $1 | sed 's/^.*$/"\0" \\/g' >> $2
 echo >> $2

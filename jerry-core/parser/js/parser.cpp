4fdb5249b99122a074f5b6adb13fcfdccf722ad6
Andrey Shitov <a.shitov@samsung.com>
2015-07-15 18:24:51 +0300

    Restore "Fix preparsing of variable declaration lists, which are not divided by a semicolon." 06d0c1806d7ceff5192feee996c770b42bc1e1e9) that was incorrectly removed in abc2b55297eff13538fca2440d127ba79cdcc8b3.
    JerryScript-DCO-1.0-Signed-off-by: Andrey Shitov a.shitov@samsung.com

--- jerry-core/parser/js/parser.cpp
+++ jerry-core/parser/js/parser.cpp
@@ -3050,6 +3050,10 @@ preparse_scope (bool is_global)
             }
             else if (token_is (TOK_KEYWORD))
             {
+              if (is_keyword (KW_VAR))
+              {
+                is_in_var_declaration_list = false;
+              }
               break;
             }
             else if (token_is (TOK_CLOSE_BRACE))

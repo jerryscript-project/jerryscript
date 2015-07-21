64c81cc44d288427e4e04349afab1a99b23e3e54
Laszlo Vidacs <lvidacs.u-szeged@partner.samsung.com>
2015-07-21 16:17:02 +0200

    Implement String.prototype.indexOf()
    JerryScript-DCO-1.0-Signed-off-by: Laszlo Vidacs lvidacs.u-szeged@partner.samsung.com

--- jerry-core/ecma/builtin-objects/ecma-builtin-string-prototype.cpp
+++ jerry-core/ecma/builtin-objects/ecma-builtin-string-prototype.cpp
@@ -307,7 +307,122 @@ ecma_builtin_string_prototype_object_index_of (ecma_value_t this_arg, /**< this 
                                                ecma_value_t arg1, /**< routine's first argument */
                                                ecma_value_t arg2) /**< routine's second argument */
 {
-  ECMA_BUILTIN_CP_UNIMPLEMENTED (this_arg, arg1, arg2);
+  ecma_completion_value_t ret_value = ecma_make_empty_completion_value ();
+
+  /* 1 */
+  ECMA_TRY_CATCH (check_coercible_val,
+                  ecma_op_check_object_coercible (this_arg),
+                  ret_value);
+
+  /* 2 */
+  ECMA_TRY_CATCH (to_str_val,
+                  ecma_op_to_string (this_arg),
+                  ret_value);
+
+  /* 3 */
+  ECMA_TRY_CATCH (search_str_val,
+                  ecma_op_to_string (arg1),
+                  ret_value);
+
+  /* 4 */
+  ECMA_OP_TO_NUMBER_TRY_CATCH (pos_num,
+                               arg2,
+                               ret_value);
+
+  /* 5 */
+  ecma_string_t *original_str_p = ecma_get_string_from_value (to_str_val);
+  const ecma_length_t original_len = ecma_string_get_length (original_str_p);
+  const lit_utf8_size_t original_size = ecma_string_get_size (original_str_p);
+
+  /* 4b, 6 */
+  ecma_length_t start = ecma_builtin_helper_string_index_normalize (pos_num, original_len);
+
+  /* 7 */
+  ecma_string_t *search_str_p = ecma_get_string_from_value (search_str_val);
+  const ecma_length_t search_len = ecma_string_get_length (search_str_p);
+  const lit_utf8_size_t search_size = ecma_string_get_size (search_str_p);
+
+  ecma_number_t *ret_num_p = ecma_alloc_number ();
+  *ret_num_p = ecma_int32_to_number (-1);
+
+  /* 8 */
+  if (search_len <= original_len)
+  {
+    if (!search_len)
+    {
+      *ret_num_p = ecma_uint32_to_number (0);
+    }
+    else
+    {
+      /* create utf8 string from original string and advance to start position */
+      MEM_DEFINE_LOCAL_ARRAY (original_str_utf8_p,
+                              original_size,
+                              lit_utf8_byte_t);
+
+      ecma_string_to_utf8_string (original_str_p,
+                                  original_str_utf8_p,
+                                  (ssize_t) (original_size));
+
+      lit_utf8_iterator_t original_it = lit_utf8_iterator_create (original_str_utf8_p, original_size);
+
+      ecma_length_t index = start;
+      lit_utf8_iterator_advance (&original_it, index);
+
+      /* create utf8 string from search string */
+      MEM_DEFINE_LOCAL_ARRAY (search_str_utf8_p,
+                              search_size,
+                              lit_utf8_byte_t);
+
+      ecma_string_to_utf8_string (search_str_p,
+                                  search_str_utf8_p,
+                                  (ssize_t) (search_size));
+
+      lit_utf8_iterator_t search_it = lit_utf8_iterator_create (search_str_utf8_p, search_size);
+
+      /* iterate original string and try to match at each position */
+      bool found = false;
+
+      while (!found && index <= original_len - search_len)
+      {
+        ecma_length_t match_len = 0;
+        lit_utf8_iterator_pos_t stored_original_pos = lit_utf8_iterator_get_pos (&original_it);
+
+        while (match_len < search_len &&
+               lit_utf8_iterator_read_next (&original_it) == lit_utf8_iterator_read_next (&search_it))
+        {
+          match_len++;
+        }
+
+        /* Check for match */
+        if (match_len == search_len)
+        {
+          *ret_num_p = ecma_uint32_to_number (index);
+          found = true;
+        }
+        else
+        {
+          /* reset iterators */
+          lit_utf8_iterator_seek_bos (&search_it);
+          lit_utf8_iterator_seek (&original_it, stored_original_pos);
+          lit_utf8_iterator_incr (&original_it);
+        }
+        index++;
+      }
+
+      MEM_FINALIZE_LOCAL_ARRAY (search_str_utf8_p);
+      MEM_FINALIZE_LOCAL_ARRAY (original_str_utf8_p);
+    }
+  }
+
+  ecma_value_t new_value = ecma_make_number_value (ret_num_p);
+  ret_value = ecma_make_normal_completion_value (new_value);
+
+  ECMA_OP_TO_NUMBER_FINALIZE (pos_num);
+  ECMA_FINALIZE (search_str_val);
+  ECMA_FINALIZE (to_str_val);
+  ECMA_FINALIZE (check_coercible_val);
+
+  return ret_value;
 } /* ecma_builtin_string_prototype_object_index_of */
 
 /**

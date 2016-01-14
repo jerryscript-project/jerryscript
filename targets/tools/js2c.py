25b83517744ebe11e6808a741fe949ef76efdf18
SaeHie Park <saehie.park@samsung.com>
2016-01-14 09:01:21 +0900

    targets: merge js2c.py into targets/tools folder * also add .PHONY to target makefiles
    JerryScript-DCO-1.0-Signed-off-by: SaeHie Park saehie.park@samsung.com

--- targets/tools/js2c.py
+++ targets/tools/js2c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2015 Samsung Electronics Co., Ltd.
+# Copyright 2015-2016 Samsung Electronics Co., Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-#  This file converts ./js/*.js to a C-array in source/jerry_targetjs.h file
+#  This file converts ./js/*.js to a C-array in ./source/jerry_targetjs.h file
 
 import sys
 import glob
@@ -45,7 +45,7 @@ def removeWhitespaces(code):
     return re.sub('\n+', '\n', re.sub('\n +', '\n', code))
 
 
-LICENSE = '''/* Copyright 2015 Samsung Electronics Co., Ltd.
+LICENSE = '''/* Copyright 2015-2016 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the \"License\");
  * you may not use this file except in compliance with the License.

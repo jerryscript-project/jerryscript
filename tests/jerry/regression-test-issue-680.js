ca0141284894ebd838fe5a293c5f3796ef0dc102
László Langó <llango.u-szeged@partner.samsung.com>
2016-02-08 12:21:16 +0100

    Add regression tests for recently closed issues
    Related issues: #123, #354, #358, #384, #447, #680, #738, #739, #743, #781, #785, #786
    
    JerryScript-DCO-1.0-Signed-off-by: László Langó llango.u-szeged@partner.samsung.com

---
+++ tests/jerry/regression-test-issue-680.js
@@ -0,0 +1,28 @@
+// Copyright 2016 Samsung Electronics Co., Ltd.
+// Copyright 2016 University of Szeged.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+try
+{
+  f1(function(a1){});
+  assert (false);
+}
+catch (e)
+{
+  assert (e instanceof ReferenceError);
+}
+
+var x = {
+foo: function () { throw new TypeError("bar"); }
+};
---
+++ tests/jerry/regression-test-issue-786.js
@@ -0,0 +1,43 @@
+// Copyright 2016 Samsung Electronics Co., Ltd.
+// Copyright 2016 University of Szeged.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+try { RegExp.prototype.toString() } catch (err) {  }
+try { Date.prototype.toTimeString() } catch (err) {  }
+try { Boolean.prototype.toString() } catch (err) {  }
+try { String.prototype.match(isNaN) } catch (err) {  }
+try { Math.round(Math.atan) } catch (err) {  }
+try { Date.prototype.getUTCDay() } catch (err) {  }
+try { toLocaleString() } catch (err) {  }
+try { Array.prototype.join(String.prototype.split) } catch (err) {  }
+try { Date.prototype.getSeconds() } catch (err) {  }
+try { isNaN.apply(Date.prototype.toISOString, String.prototype.split) } catch (err) {  }
+try { Math.cos(Object) } catch (err) {  }
+try { Math.floor(Date.prototype.getUTCSeconds) } catch (err) {  }
+try { isNaN.apply(assert, JSON.stringify) } catch (err) {  }
+try { Number.prototype.toLocaleString() } catch (err) {  }
+try { Date.prototype.getTimezoneOffset() } catch (err) {  }
+try { Date.prototype.setMinutes(Object.getOwnPropertyDescriptor,  Object.getOwnPropertyNames) } catch (err) {  }
+try { Math.log(String.prototype.search) } catch (err) {  }
+try { ReferenceError(Object.defineProperty) } catch (err) {  }
+try { Array.prototype.filter(Number.prototype.toLocaleString) } catch (err) {  }
+try { Array.prototype.forEach(Date.prototype.getDay) } catch (err) {  }
+try { Object.defineProperty(SyntaxError.prototype, Date.prototype.setUTCMinutes, Math.max) } catch (err) {  }
+try { Date.prototype.setMilliseconds(String.prototype.localeCompare) } catch (err) {  }
+try { Math.asin(Date.prototype.getDay) } catch (err) {  }
+try { Array.prototype.some(Date.prototype.getDay) } catch (err) {  }
+try { String.prototype.charCodeAt(toString) } catch (err) {  }
+try { isPrototypeOf(Math.tan) } catch (err) {  }
+try { Object.isFrozen(print) } catch (err) {  }
+try { RegExp.prototype.compile(RegExp.prototype) } catch (err) {  }

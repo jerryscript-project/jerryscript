b6cbe914ab162c172ce151b42ed2ea95c6159e0f
Imre Kiss <kissi@inf.u-szeged.hu>
2016-04-01 11:47:44 +0200

    New target for Discovery STM32F419ZI board Update the JerryScript API changes. Delete the extended print implementation. Fix the DECLARE_HANDLER name erratum. Correct some coding style issue. Check and release the err_obj_p object. Update jerry_port.c in the target source.
    JerryScript-DCO-1.0-Signed-off-by: Imre Kiss kissi@inf.u-szeged.hu

---
+++ targets/mbedstm32f429i/source/jerry_port.c
@@ -0,0 +1,55 @@
+/* Copyright 2014-2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdarg.h>
+
+#include "jerry-core/jerry.h"
+
+/**
+ * Provide log message to filestream implementation for the engine.
+ */
+int jerry_port_logmsg (FILE *stream, /**< stream pointer */
+                       const char *format, /**< format string */
+                       ...) /**< parameters */
+{
+  va_list args;
+  int count;
+  va_start (args, format);
+  count = vfprintf (stream, format, args);
+  va_end (args);
+  return count;
+} /* jerry_port_logmsg */
+
+/**
+ * Provide error message to console implementation for the engine.
+ */
+int jerry_port_errormsg (const char *format, /**< format string */
+                         ...) /**< parameters */
+{
+  va_list args;
+  int count;
+  va_start (args, format);
+  count = vfprintf (stderr, format, args);
+  va_end (args);
+  return count;
+} /* jerry_port_errormsg */
+
+/**
+ * Provide output character to console implementation for the engine.
+ */
+int jerry_port_putchar (int c) /**< character to put */
+{
+  return putchar ((unsigned char) c);
+} /* jerry_port_putchar */

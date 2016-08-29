ba2b7dd13f2cd8dcc25975bc5c757b1330a16bcf
Yanhui Shen <shen.elf@gmail.com>
2016-08-29 22:31:21 +0800

    Use C99's variable length array instead of alloca
    JerryScript-DCO-1.0-Signed-off-by: Yanhui Shen shen.elf@gmail.com

--- jerry-core/vm/vm.c
+++ jerry-core/vm/vm.c
@@ -36,8 +36,6 @@
 #include "vm.h"
 #include "vm-stack.h"
 
-#include <alloca.h>
-
 /** \addtogroup vm Virtual machine
  * @{
  *
@@ -2635,9 +2633,7 @@ vm_run_with_alloca (vm_frame_ctx_t *frame_ctx_p, /**< frame context */
                     ecma_length_t arg_list_len, /**< length of arguments list */
                     uint32_t call_stack_size) /**< call stack size */
 {
-  size_t size = call_stack_size * sizeof (ecma_value_t);
-
-  ecma_value_t *stack = (ecma_value_t *) alloca (size);
+  ecma_value_t stack[call_stack_size];
 
   frame_ctx_p->registers_p = stack;
 

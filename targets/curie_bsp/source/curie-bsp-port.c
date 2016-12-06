16b8ccdab0937f31f9fa18c7660535d5698f1ab8
Tilmann Scheller <t.scheller@samsung.com>
2016-12-06 11:53:05 +0100

    Streamline copyright notices across the codebase.
    Since the project is now hosted at the JS Foundation we can move to unified copyright notices for the project.
    
    Starting with this commit all future contributions to the project should only carry the following copyright notice (except for third-party code which requires copyright information to be preserved):
    
    "Copyright JS Foundation and other contributors, http://js.foundation" (without the quotes)
    
    This avoids cluttering the codebase with contributor-specific copyright notices which have a higher maintenance overhead and tend to get outdated quickly. Also dropping the year from the copyright notices helps to avoid yearly code changes just to update the copyright notices.
    
    Note that each contributor still retains full copyright ownership of his/her contributions and the respective authorship is tracked very accurately via Git.
    
    JerryScript-DCO-1.0-Signed-off-by: Tilmann Scheller t.scheller@samsung.com

--- targets/curie_bsp/source/curie-bsp-port.c
+++ targets/curie_bsp/source/curie-bsp-port.c
@@ -1,4 +1,4 @@
-/* Copyright 2016 Intel Corporation
+/* Copyright JS Foundation and other contributors, http://js.foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

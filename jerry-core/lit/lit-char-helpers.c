8f3f65f9b3ea93e5d888566d4ccec0307cae25b8
Robert Sipka <rsipka.uszeged@partner.samsung.com>
2017-02-06 09:12:25 +0100

    Improve `toLowerCase` and `toUpperCase` functions.
    Language-sensitive mappings are not processed now.
    Fixes #323
    
    JerryScript-DCO-1.0-Signed-off-by: Robert Sipka rsipka.uszeged@partner.samsung.com

--- jerry-core/lit/lit-char-helpers.c
+++ jerry-core/lit/lit-char-helpers.c
@@ -17,6 +17,10 @@
 #include "lit/lit-unicode-ranges.inc.h"
 #include "lit-strings.h"
 
+#ifndef CONFIG_DISABLE_UNICODE_CASE_CONVERSION
+#include "lit-unicode-conversions.inc.h"
+#endif /* !CONFIG_DISABLE_UNICODE_CASE_CONVERSION */
+
 #define NUM_OF_ELEMENTS(array) (sizeof (array) / sizeof ((array)[0]))
 
 /**
@@ -458,6 +462,184 @@ lit_char_is_word_char (ecma_char_t c) /**< code unit */
           || c == LIT_CHAR_UNDERSCORE);
 } /* lit_char_is_word_char */
 
+#ifndef CONFIG_DISABLE_UNICODE_CASE_CONVERSION
+
+/**
+ * Check if the specified character is in one of those tables which contain bidirectional conversions.
+ *
+ * @return the mapped character sequence of an ecma character, if it's in the table.
+ *         0 - otherwise.
+ */
+static ecma_length_t
+search_in_bidirectional_conversion_tables (ecma_char_t character,        /**< code unit */
+                                           ecma_char_t *output_buffer_p, /**< [out] buffer for the result characters */
+                                           bool is_lowercase)            /**< is lowercase conversion */
+{
+  /* 1, Check if the specified character is part of the jerry_character_case_ranges table. */
+  int number_of_case_ranges = NUM_OF_ELEMENTS (jerry_character_case_ranges);
+  int conv_counter = 0;
+
+  for (int i = 0; i < number_of_case_ranges; i++)
+  {
+    if (i % 2 == 0 && i > 0)
+    {
+      conv_counter++;
+    }
+
+    int range_length = jerry_character_case_range_lengths[conv_counter];
+    ecma_char_t start_point = jerry_character_case_ranges[i];
+
+    if (start_point > character || character >= start_point + range_length)
+    {
+      continue;
+    }
+
+    int char_dist = character - start_point;
+
+    if (i % 2 == 0)
+    {
+      output_buffer_p[0] = is_lowercase ? (ecma_char_t) (jerry_character_case_ranges[i + 1] + char_dist) : character;
+    }
+    else
+    {
+      output_buffer_p[0] = is_lowercase ? character : (ecma_char_t) (jerry_character_case_ranges[i - 1] + char_dist);
+    }
+
+    return 1;
+  }
+
+  /* 2, Check if the specified character is part of the character_pair_ranges table. */
+  int bottom = 0;
+  int top = NUM_OF_ELEMENTS (jerry_character_pair_ranges) - 1;
+
+  while (bottom <= top)
+  {
+    int middle = (bottom + top) / 2;
+    ecma_char_t current_sp = jerry_character_pair_ranges[middle];
+
+    if (current_sp <= character && character < current_sp + jerry_character_pair_range_lengths[middle])
+    {
+      int char_dist = character - current_sp;
+
+      if ((character - current_sp) % 2 == 0)
+      {
+        output_buffer_p[0] = is_lowercase ? (ecma_char_t) (current_sp + char_dist + 1) : character;
+      }
+      else
+      {
+        output_buffer_p[0] = is_lowercase ? character : (ecma_char_t) (current_sp + char_dist - 1);
+      }
+
+      return 1;
+    }
+
+    if (character > current_sp)
+    {
+      bottom = middle + 1;
+    }
+    else
+    {
+      top = middle - 1;
+    }
+  }
+
+  /* 3, Check if the specified character is part of the character_pairs table. */
+  int number_of_character_pairs = NUM_OF_ELEMENTS (jerry_character_pairs);
+
+  for (int i = 0; i < number_of_character_pairs; i++)
+  {
+    if (character != jerry_character_pairs[i])
+    {
+      continue;
+    }
+
+    if (i % 2 == 0)
+    {
+      output_buffer_p[0] = is_lowercase ? jerry_character_pairs[i + 1] : character;
+    }
+    else
+    {
+      output_buffer_p[0] = is_lowercase ? character : jerry_character_pairs[i - 1];
+    }
+
+    return 1;
+  }
+
+  return 0;
+} /* search_in_bidirectional_conversion_tables */
+
+/**
+ * Check if the specified character is in the given conversion table.
+ *
+ * @return the mapped character sequence of an ecma character, if it's in the table.
+ *         0 - otherwise.
+ */
+static ecma_length_t
+search_in_conversion_table (ecma_char_t character,        /**< code unit */
+                            ecma_char_t *output_buffer_p, /**< [out] buffer for the result characters */
+                            const ecma_char_t *array,     /**< array */
+                            const uint8_t *counters)      /**< case_values counter */
+{
+  int end_point = 0;
+
+  for (int i = 0; i < 3; i++)
+  {
+    int start_point = end_point;
+    int size_of_case_value = i + 1;
+    end_point += counters[i] * (size_of_case_value + 1);
+
+    int bottom = start_point;
+    int top = end_point - size_of_case_value;
+
+    while (bottom <= top)
+    {
+      int middle = (bottom + top) / 2;
+
+      middle -= ((middle - bottom) % (size_of_case_value + 1));
+
+      ecma_char_t current = array[middle];
+
+      if (current == character)
+      {
+        ecma_length_t char_sequence = 1;
+
+        switch (size_of_case_value)
+        {
+          case 3:
+          {
+            output_buffer_p[2] = array[middle + 3];
+            char_sequence++;
+            /* FALLTHRU */
+          }
+          case 2:
+          {
+            output_buffer_p[1] = array[middle + 2];
+            char_sequence++;
+            /* FALLTHRU */
+          }
+          default:
+          {
+            output_buffer_p[0] = array[middle + 1];
+            return char_sequence;
+          }
+        }
+      }
+
+      if (character < current)
+      {
+        top = middle - (size_of_case_value + 1);
+      }
+      else
+      {
+        bottom = middle + (size_of_case_value + 1);
+      }
+    }
+  }
+
+  return 0;
+} /* search_in_conversion_table */
+#endif /* !CONFIG_DISABLE_UNICODE_CASE_CONVERSION */
+
 /**
  * Returns the lowercase character sequence of an ecma character.
  *
@@ -471,8 +653,6 @@ lit_char_to_lower_case (ecma_char_t character, /**< input character value */
                         ecma_char_t *output_buffer_p, /**< [out] buffer for the result characters */
                         ecma_length_t buffer_size) /**< buffer size */
 {
-  /* TODO: Needs a proper lower case implementation. See issue #323. */
-
   JERRY_ASSERT (buffer_size >= LIT_MAXIMUM_OTHER_CASE_LENGTH);
 
   if (character >= LIT_CHAR_UPPERCASE_A && character <= LIT_CHAR_UPPERCASE_Z)
@@ -480,6 +660,41 @@ lit_char_to_lower_case (ecma_char_t character, /**< input character value */
     output_buffer_p[0] = (ecma_char_t) (character + (LIT_CHAR_LOWERCASE_A - LIT_CHAR_UPPERCASE_A));
     return 1;
   }
+
+#ifndef CONFIG_DISABLE_UNICODE_CASE_CONVERSION
+
+  ecma_length_t lowercase_sequence = search_in_bidirectional_conversion_tables (character, output_buffer_p, true);
+
+  if (lowercase_sequence != 0)
+  {
+    return lowercase_sequence;
+  }
+
+  int num_of_lowercase_ranges = NUM_OF_ELEMENTS (jerry_lower_case_ranges);
+
+  for (int i = 0, j = 0; i < num_of_lowercase_ranges; i += 2, j++)
+  {
+    int range_length = jerry_lower_case_range_lengths[j] - 1;
+    ecma_char_t start_point = jerry_lower_case_ranges[i];
+
+    if (start_point <= character && character <= start_point + range_length)
+    {
+      output_buffer_p[0] = (ecma_char_t) (jerry_lower_case_ranges[i + 1] + (character - start_point));
+      return 1;
+    }
+  }
+
+  lowercase_sequence = search_in_conversion_table (character,
+                                                   output_buffer_p,
+                                                   jerry_lower_case_conversions,
+                                                   jerry_lower_case_conversion_counters);
+
+  if (lowercase_sequence != 0)
+  {
+    return lowercase_sequence;
+  }
+
+#endif /* !CONFIG_DISABLE_UNICODE_CASE_CONVERSION */
 
   output_buffer_p[0] = character;
   return 1;
@@ -498,8 +713,6 @@ lit_char_to_upper_case (ecma_char_t character, /**< input character value */
                         ecma_char_t *output_buffer_p, /**< buffer for the result characters */
                         ecma_length_t buffer_size) /**< buffer size */
 {
-  /* TODO: Needs a proper upper case implementation. See issue #323. */
-
   JERRY_ASSERT (buffer_size >= LIT_MAXIMUM_OTHER_CASE_LENGTH);
 
   if (character >= LIT_CHAR_LOWERCASE_A && character <= LIT_CHAR_LOWERCASE_Z)
@@ -507,6 +720,42 @@ lit_char_to_upper_case (ecma_char_t character, /**< input character value */
     output_buffer_p[0] = (ecma_char_t) (character - (LIT_CHAR_LOWERCASE_A - LIT_CHAR_UPPERCASE_A));
     return 1;
   }
+
+#ifndef CONFIG_DISABLE_UNICODE_CASE_CONVERSION
+
+  ecma_length_t uppercase_sequence = search_in_bidirectional_conversion_tables (character, output_buffer_p, false);
+
+  if (uppercase_sequence != 0)
+  {
+    return uppercase_sequence;
+  }
+
+  int num_of_upper_case_special_ranges = NUM_OF_ELEMENTS (jerry_upper_case_special_ranges);
+
+  for (int i = 0, j = 0; i < num_of_upper_case_special_ranges; i += 3, j++)
+  {
+    int range_length = jerry_upper_case_special_range_lengths[j];
+    ecma_char_t start_point = jerry_upper_case_special_ranges[i];
+
+    if (start_point <= character && character <= start_point + range_length)
+    {
+      output_buffer_p[0] = (ecma_char_t) (jerry_upper_case_special_ranges[i + 1] + (character - start_point));
+      output_buffer_p[1] = (ecma_char_t) (jerry_upper_case_special_ranges[i + 2]);
+      return 2;
+    }
+  }
+
+  uppercase_sequence = search_in_conversion_table (character,
+                                                   output_buffer_p,
+                                                   jerry_upper_case_conversions,
+                                                   jerry_upper_case_conversion_counters);
+
+  if (uppercase_sequence != 0)
+  {
+    return uppercase_sequence;
+  }
+
+#endif /* !CONFIG_DISABLE_UNICODE_CASE_CONVERSION */
 
   output_buffer_p[0] = character;
   return 1;
--- tests/jerry/string-upper-lower-case-conversion.js
+++ tests/jerry/string-upper-lower-case-conversion.js
@@ -12,7 +12,72 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Conversion
+// LATIN SMALL LIGATURES
+// LATIN SMALL LIGATURE FF
+assert ("\ufb00".toLowerCase() == "\ufb00");
+assert ("\ufb00".toUpperCase() == "\u0046\u0046");
+// LATIN SMALL LIGATURE FI
+assert ("\ufb01".toLowerCase() == "\ufb01");
+assert ("\ufb01".toUpperCase() == "\u0046\u0049");
+// LATIN SMALL LIGATURE FL
+assert ("\ufb02".toLowerCase() == "\ufb02");
+assert ("\ufb02".toUpperCase() == "\u0046\u004c");
+// LATIN SMALL LIGATURE FFI
+assert ("\ufb03".toLowerCase() == "\ufb03");
+assert ("\ufb03".toUpperCase() == "\u0046\u0046\u0049");
+// LATIN SMALL LIGATURE FFL
+assert ("\ufb04".toLowerCase() == "\ufb04");
+assert ("\ufb04".toUpperCase() == "\u0046\u0046\u004c");
+// LATIN SMALL LIGATURE LONG S T
+assert ("\ufb05".toLowerCase() == "\ufb05");
+assert ("\ufb05".toUpperCase() == "\u0053\u0054");
+// LATIN SMALL LIGATURE ST
+assert ("\ufb06".toLowerCase() == "\ufb06");
+assert ("\ufb06".toUpperCase() == "\u0053\u0054");
+
+// LATIN CAPITAL LETTER I WITH DOT ABOVE
+assert ("\u0130".toLowerCase() == "\u0069\u0307");
+assert ("\u0130".toUpperCase() == "\u0130");
+
+// LATIN SMALL LETTER SHARP S
+assert ("\u00df".toLowerCase() == "\u00df");
+assert ("\u00df".toUpperCase() == "\u0053\u0053");
+
+// LATIN CAPITAL LETTER I WITH BREVE
+assert ("\u012c".toLowerCase() == "\u012d");
+assert ("\u012c".toUpperCase() == "\u012c");
+// LATIN SMALL LETTER I WITH BREVE
+assert ("\u012d".toLowerCase() == "\u012d")
+assert ("\u012d".toUpperCase() == "\u012c");
+
+// Check randomly selected characters from conversion tables
+
+// lower-case conversions
+assert ("\u01c5\u01c8\u01cb\u212b".toLowerCase() == "\u01c6\u01c9\u01cc\u00e5");
+assert ("\u0130".toLowerCase() == "\u0069\u0307");
+
+// upper-case conversions
+assert ("\u00b5\u017f".toUpperCase() == "\u039c\u0053");
+assert ("\ufb17\u00df\u1fbc".toUpperCase() == "\u0544\u053D\u0053\u0053\u0391\u0399");
+assert ("\ufb03\ufb04".toUpperCase() == "\u0046\u0046\u0049\u0046\u0046\u004c");
+
+// character case ranges
+assert ("\u0100\u0101\u0139\u03fa\ua7b4".toLowerCase() == "\u0101\u0101\u013a\u03fb\ua7b5");
+assert ("\u0101\u0100\u013a\u03fb\ua7b5".toUpperCase() == "\u0100\u0100\u0139\u03fa\ua7b4");
+
+// character pairs
+assert ("\u0178\ua7b1\u0287\ua7b3".toLowerCase() == "\u00ff\u0287\u0287\uab53");
+assert ("\u00ff\u0287\ua7b1\uab53".toUpperCase() == "\u0178\ua7b1\ua7b1\ua7b3");
+
+// character case ranges
+assert ("\u00e0\u00c0\u00c1\u00c2\uff21".toLowerCase() == "\u00e0\u00e0\u00e1\u00e2\uff41");
+assert ("\u00e0\u00c0\u00e1\u00e2\uff41".toUpperCase() == "\u00c0\u00c0\u00c1\u00c2\uff21");
+
+// lower-case ranges
+assert ("\u1f88\u1f98\u1fa8\u1f8b\u1faf".toLowerCase() == "\u1f80\u1f90\u1fa0\u1f83\u1fa7");
+
+// upper-case special ranges
+assert ("\u1f80\u1f81\u1fa7".toUpperCase() == "\u1f08\u0399\u1f09\u0399\u1f6f\u0399");
 
 assert ("0123456789abcdefghijklmnopqrstuvwxzyABCDEFGHIJKLMNOPQRSTUVWXYZ".toLowerCase()
         == "0123456789abcdefghijklmnopqrstuvwxzyabcdefghijklmnopqrstuvwxyz");

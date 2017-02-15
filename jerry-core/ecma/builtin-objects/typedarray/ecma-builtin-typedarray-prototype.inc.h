545e2be6386f45805ea3866d8e379ecd854c780d
Zidong Jiang <zidong.jiang@intel.com>
2017-02-15 18:30:03 +0800

    add typedarray routine:reverse
    JerryScript-DCO-1.0-Signed-off-by: Zidong Jiang zidong.jiang@intel.com

--- jerry-core/ecma/builtin-objects/typedarray/ecma-builtin-typedarray-prototype.inc.h
+++ jerry-core/ecma/builtin-objects/typedarray/ecma-builtin-typedarray-prototype.inc.h
@@ -63,6 +63,7 @@ ROUTINE (LIT_MAGIC_STRING_MAP, ecma_builtin_typedarray_prototype_map, 2, 1)
 ROUTINE (LIT_MAGIC_STRING_REDUCE, ecma_builtin_typedarray_prototype_reduce, 2, 1)
 ROUTINE (LIT_MAGIC_STRING_REDUCE_RIGHT_UL, ecma_builtin_typedarray_prototype_reduce_right, 2, 1)
 ROUTINE (LIT_MAGIC_STRING_FILTER, ecma_builtin_typedarray_prototype_filter, 2, 1)
+ROUTINE (LIT_MAGIC_STRING_REVERSE, ecma_builtin_typedarray_prototype_reverse, 0, 0)
 
 #undef SIMPLE_VALUE
 #undef NUMBER_VALUE

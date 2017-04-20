00d8fcb625668b1d23f800ffa9fa00243a2b6337
Gabriel Schulhof <gabriel.schulhof@intel.com>
2017-04-20 11:22:48 +0300

    Add new extension jerry-context
    This is a user context manager that expands the number of pointers that
    can be attached to a user context from one to a number specified at
    compiled time.
    
    It reuses the core interface whereby an init/deinit pair is passed and
    only a getter is provided for the value created by the init and stored
    in the context.
    
    JerryScript-DCO-1.0-Signed-off-by: Gabriel Schulhof gabriel.schulhof@intel.com

---
+++ jerry-ext/jerry-context/jerry-context.c
@@ -0,0 +1,139 @@
+/* Copyright JS Foundation and other contributors, http://js.foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <string.h>
+#include "jerry-context.h"
+#include "jmem.h"
+
+#define HEAP_BLOCK_SIZE (JERRYX_CONTEXT_SLOTS * sizeof (void *))
+
+typedef struct
+{
+  jerry_user_context_init_cb init_cb;
+  jerry_user_context_deinit_cb deinit_cb;
+} jerryx_context_slot_info;
+
+/* Context-free data */
+
+/* We do not allocate slots after a context has already been created. */
+bool before_first_context = true;
+static int next_index = -1;
+static jerryx_context_slot_info slot_info[JERRYX_CONTEXT_SLOTS] =
+{
+  {
+    NULL, NULL
+  }
+};
+
+/**
+ * Create a new context. This function can be passed to jerry_init_with_user_context () as its second parameter.
+ *
+ * @return the pointer to store as the user context.
+ */
+void *
+jerryx_context_init (void)
+{
+  size_t index = 0;
+  void **return_value = NULL;
+
+  before_first_context = false;
+
+  return_value = (void **) jmem_heap_alloc_block_null_on_error (HEAP_BLOCK_SIZE);
+
+  if (return_value)
+  {
+    memset (return_value, 0, HEAP_BLOCK_SIZE);
+    for (index = 0; index < JERRYX_CONTEXT_SLOTS; index++)
+    {
+      if (slot_info[index].init_cb)
+      {
+        return_value[index] = slot_info[index].init_cb ();
+      }
+    }
+  }
+
+  return ((void *) return_value);
+} /* jerryx_context_init */
+
+/**
+ * Free a context. This function can be passed to jerry_init_with_user_context () as its third parameter.
+ */
+void
+jerryx_context_deinit (void *user_context_p) /**< user context to deinit */
+{
+  size_t index;
+  void **slots = NULL;
+
+  if (user_context_p)
+  {
+    slots = (void **) user_context_p;
+    for (index = 0; index < JERRYX_CONTEXT_SLOTS; index++)
+    {
+      if (slot_info[index].deinit_cb)
+      {
+        slot_info[index].deinit_cb (slots[index]);
+      }
+    }
+    jmem_heap_free_block (slots, HEAP_BLOCK_SIZE);
+  }
+} /* jerryx_context_deinit */
+
+/**
+ * Request an index for per-context data.
+ *
+ * @return the slot in which the user data will be stored, or -1 on failure. The value can later be passed to
+ * jerryx_context_get_slot () to retrieve the per-context user data.
+ */
+int
+jerryx_context_request_slot (jerry_user_context_init_cb init_cb, /**< callback for creating a new context */
+                             jerry_user_context_deinit_cb deinit_cb) /**< callback for freeing context */
+{
+  int return_value = -1;
+
+  if (before_first_context)
+  {
+    return_value = (++next_index);
+    if (return_value >= JERRYX_CONTEXT_SLOTS)
+    {
+      return_value = -1;
+    }
+    else
+    {
+      slot_info[return_value].init_cb = init_cb;
+      slot_info[return_value].deinit_cb = deinit_cb;
+    }
+  }
+
+  return return_value;
+} /* jerryx_context_request_slot */
+
+/**
+ * Request the slot at the given index.
+ *
+ * @return the user data that was created in the given slot when the context was initialized.
+ */
+void *
+jerryx_context_get_slot (int slot) /**< the slot from which to retrieve the data */
+{
+  void *return_value = NULL;
+  void **slots = (void **) jerry_get_user_context ();
+
+  if (slots && slot >= 0 && slot < JERRYX_CONTEXT_SLOTS)
+  {
+    return_value = slots[slot];
+  }
+
+  return return_value;
+} /* jerryx_context_get_slot */

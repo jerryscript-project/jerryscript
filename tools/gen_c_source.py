ca97c7e27904536b5e98a143f6cf487ce278a422
Akos Kiss <akiss@inf.u-szeged.hu>
2017-04-21 16:12:58 +0200

    Merge `lit-unicode-*.inc.h`-related generator scripts
    The `conversions` and `ranges` files had separate generators, which
    shared some functionality through a 3rd python module, and were
    quite similar to each other -- but also had some differences.
    Moreover, as they were separate scripts, nothing forced to keep the
    outputs in sync (to re-generate them at the same time from the same
    inputs).
    
    To fix these maintainability issues, this patch
    - merges the two python files and part of the 3rd utility module
      into a single script while keeping the existing functionality,
    - names the new file `gen-unicode.py` to align better with other
      generator script in `tools`, and
    - adds some extra documentation how to retrieve the input files
      (as they are not part of the repository).
    
    The refactoring affected the utility module as well, so this patch
    also
    - renames `c_source_helper.py` to `gen_c_source.py` (again, for
      naming consistency),
    - reorganizes some of its functions not to export module-local
      helper functions, and
    - adapts `js2c.py`, as it also uses this utility module.
    
    JerryScript-DCO-1.0-Signed-off-by: Akos Kiss akiss@inf.u-szeged.hu

--- tools/gen_c_source.py
+++ tools/gen_c_source.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 # Copyright JS Foundation and other contributors, http://js.foundation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -31,42 +29,16 @@ LICENSE = """/* Copyright JS Foundation and other contributors, http://js.founda
  */"""
 
 
-class UniCodeSource(object):
-    def __init__(self, filepath):
-        self.__filepath = filepath
-        self.__header = [LICENSE, ""]
-        self.__data = []
-
-    def complete_header(self, completion):
-        self.__header.append(completion)
-        self.__header.append("")  # for an extra empty line
-
-    def add_table(self, table, table_name, table_type, table_descr):
-        self.__data.append(table_descr)
-        self.__data.append("static const %s jerry_%s[] JERRY_CONST_DATA =" % (table_type, table_name))
-        self.__data.append("{")
-        self.__data.append(format_code(table, 1))
-        self.__data.append("};")
-        self.__data.append("")  # for an extra empty line
-
-    def generate(self):
-        with open(self.__filepath, 'w') as generated_source:
-            generated_source.write("\n".join(self.__header))
-            generated_source.write("\n".join(self.__data))
-
-
-def regroup(list_to_group, num):
-    return [list_to_group[i:i+num] for i in range(0, len(list_to_group), num)]
-
-
-def hex_format(char, digit_number):
-    if isinstance(char, str):
-        char = ord(char)
-
-    return ("0x{:0%sx}" % digit_number).format(char)
-
-
 def format_code(code, indent, digit_number=4):
+    def regroup(list_to_group, num):
+        return [list_to_group[i:i+num] for i in range(0, len(list_to_group), num)]
+
+    def hex_format(char, digit_number):
+        if isinstance(char, str):
+            char = ord(char)
+
+        return ("0x{:0%sx}" % digit_number).format(char)
+
     lines = []
 
     nums_per_line = 10

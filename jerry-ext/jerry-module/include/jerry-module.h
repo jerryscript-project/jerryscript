9560aef3f0e956dfe0c3b9ade1723f55a6d9039d
Gabriel Schulhof <gabriel.schulhof@intel.com>
2017-04-24 17:23:37 +0300

    Add a new extension jerry-module
    This is a module manager. Modules consist of one or more source files
    with a well-defined entry point and identified by a name (a string).
    Module functionality requires FEATURE_INIT_FINI, and integrates well
    with jerry-context, requiring one slot. Modules behave as follows:
    
    1. The module includes a constructor function and a global static
       structure that defines the module:
       - the module name,
       - the module API version, and
       - the module's entry point - that is, the function that will be
         called in order to retrieve the module's exported JavaScript value.
    
    2. The action of the constructor is to pass a pointer to the global
       static structure to jerry-module via `jerry_module_register ()`.
    
    3. jerry-module arranges the global static structures it receives via
       `jerry_module_register ()` into a global static list of modules.
    
    4. modules are loaded into a context via `jerry_module_load ()`, which
       looks up the module by name from the list of available modules and
       calls the module's entry point. The resulting `jerry_value_t` is
       cached in the context's user pointer, if available.
    
       The context user pointer can be initialized by passing the functions
       `jerry_module_context_init ()` and `jerry_module_context_deinit ()`
       to `jerry_init_with_user_context ()`. When the jerry-context
       extension is also built jerry-module will automatically request a
       slot from jerry-context at startup via its own constructor.
    
    JerryScript-DCO-1.0-Signed-off-by: Gabriel Schulhof gabriel.schulhof@intel.com

---
+++ jerry-ext/jerry-module/include/jerry-module.h
@@ -0,0 +1,82 @@
+#ifndef JERRYX_MODULE_H
+#define JERRYX_MODULE_H
+
+#include "jerryscript.h"
+
+#ifdef __cplusplus
+#define EXTERN_C_START extern "C" {
+#define EXTERN_C_END }
+#else
+#define EXTERN_C_START
+#define EXTERN_C_END
+#endif
+
+#define JERRYX_C_CTOR(fn)                           \
+static void fn(void) __attribute__((constructor)); \
+static void fn(void)
+
+#define JERRYX_MODULE_VERSION 1
+
+typedef struct jerryx_module_link
+{
+  jerry_char_t *name;
+  struct jerryx_module_link *next;
+} jerryx_module_link_t;
+
+#define JERRYX_MODULE_LINK_STATIC_INIT(name) \
+  {name, NULL}
+
+#define JERRYX_MODULE_LINK_FLOAT(link) \
+  ((jerryx_module_link_t *) link)->next = NULL
+
+#define JERRYX_MODULE_LINK_RUNTIME_INIT(link, module_name) \
+  ((jerryx_module_link_t *) link)->name = (module_name);   \
+  JERRYX_MODULE_LINK_FLOAT((link))
+
+typedef struct
+{
+  jerryx_module_link_t link;
+  int version;
+  jerry_value_t (*init)(void);
+} jerryx_module_t;
+
+/**
+ * Function to create a module manager context
+ */
+void *jerryx_module_context_init (void);
+
+/**
+ * Function to clean up a given module manager context
+ */
+void jerryx_module_context_deinit (void *context_p);
+
+/**
+ * Function to register a module
+ */
+void jerryx_module_register (jerryx_module_t *module);
+
+/**
+ * Function to unregister a module
+ */
+void jerryx_module_unregister (jerryx_module_t *module);
+
+/**
+ * Function to load a copy of a module into the current context
+ */
+jerry_value_t jerryx_module_load (const jerry_char_t *name);
+
+#define JERRYX_MODULE(name, init)                         \
+EXTERN_C_START                                            \
+static jerryx_module_t _module =                          \
+{                                                         \
+  JERRYX_MODULE_LINK_STATIC_INIT((jerry_char_t *) #name), \
+  JERRYX_MODULE_VERSION,                                  \
+  (init)                                                  \
+};                                                        \
+JERRYX_C_CTOR(_register_ ## name)                         \
+{                                                         \
+  jerryx_module_register(&_module);                       \
+}                                                         \
+EXTERN_C_END
+
+#endif /* !JERRYX_MODULE_H */
---
+++ jerry-ext/jerry-module/jerry-module.c
@@ -0,0 +1,198 @@
+#include "jmem.h"
+#include "jerryscript.h"
+#include "jerry-module.h"
+
+static void
+jerryx_module_link_insert (jerryx_module_link_t *link,
+                           jerryx_module_link_t **root)
+{
+  if (*root)
+  {
+    link->next = (*root);
+  }
+  (*root) = link;
+} /* jerryx_module_link_insert */
+
+static jerryx_module_link_t *
+jerryx_module_link_find (jerryx_module_link_t *root,
+                         const jerry_char_t *name,
+                         jerryx_module_link_t **previous)
+{
+  if (!root)
+  {
+    return NULL;
+  }
+  if (strcmp ((char *) name, ((char *) root->name)))
+  {
+    if (previous)
+    {
+      (*previous) = root;
+    }
+    return jerryx_module_link_find (root->next, name, previous);
+  }
+  else
+  {
+    return root;
+  }
+} /* jerryx_module_link_find */
+
+static jerryx_module_link_t *
+jerryx_module_link_remove (const jerry_char_t *name,
+                           jerryx_module_link_t **root)
+{
+  jerryx_module_link_t *previous = NULL;
+  jerryx_module_link_t *return_value = jerryx_module_link_find ((*root), name, &previous);
+
+  if (return_value)
+  {
+    if (previous)
+    {
+      previous->next = return_value->next;
+    }
+    else
+    {
+      (*root) = return_value->next;
+    }
+    JERRYX_MODULE_LINK_FLOAT (return_value);
+  }
+
+  return return_value;
+} /* jerryx_module_link_remove */
+
+static void
+jerryx_module_link_free (jerryx_module_link_t *root,
+                         void (*free_link) (jerryx_module_link_t *link))
+{
+  jerryx_module_link_t *next;
+  while (root)
+  {
+    next = root->next;
+    free_link (root);
+    root = next;
+  }
+} /* jerryx_module_link_free */
+
+typedef struct
+{
+  jerryx_module_link_t link;
+  jerry_value_t result;
+} jerryx_module_instance_t;
+
+static void
+jerryx_module_free_instance (jerryx_module_link_t *link)
+{
+  jerryx_module_instance_t *instance = (jerryx_module_instance_t *) link;
+  jerry_release_value (instance->result);
+  jmem_heap_free_block (instance, sizeof (jerryx_module_instance_t));
+} /* jerryx_module_free_instance */
+
+void *
+jerryx_module_context_init (void)
+{
+  void *return_value = jmem_heap_alloc_block_null_on_error (sizeof (jerryx_module_instance_t *));
+  memset (return_value, 0, sizeof (jerryx_module_instance_t *));
+  return return_value;
+} /* jerryx_module_context_init */
+
+void
+jerryx_module_context_deinit (void *context_p)
+{
+  if (context_p)
+  {
+    jerryx_module_link_free (*((jerryx_module_link_t **) (context_p)), jerryx_module_free_instance);
+    jmem_heap_free_block (context_p, sizeof (jerryx_module_link_t *));
+  }
+} /* jerryx_module_context_deinit */
+
+#ifdef JERRYX_MODULE_HAVE_CONTEXT
+#include "jerry-context.h"
+static int jerryx_module_user_context_slot = -1;
+JERRYX_C_CTOR (_register_module_handler_)
+{
+  jerryx_module_user_context_slot = jerryx_context_request_slot (jerryx_module_context_init,
+                                                                 jerryx_module_context_deinit);
+} /* JERRYX_C_CTOR */
+#define JERRYX_MODULE_CONTEXT \
+  (jerryx_context_get_slot (jerryx_module_user_context_slot))
+#else /* !JERRYX_MODULE_HAVE_CONTEXT */
+#define JERRYX_MODULE_CONTEXT \
+  (jerry_get_user_context ())
+#endif /* JERRYX_MODULE_HAVE_CONTEXT */
+
+/* Context-free list of modules */
+static jerryx_module_t *modules = NULL;
+
+void
+jerryx_module_register (jerryx_module_t *module)
+{
+  jerryx_module_link_insert ((jerryx_module_link_t *) module, ((jerryx_module_link_t **) (&modules)));
+} /* jerryx_module_register */
+
+void
+jerryx_module_unregister (jerryx_module_t *module)
+{
+  jerryx_module_link_remove (module->link.name, ((jerryx_module_link_t **) (&modules)));
+} /* jerryx_module_unregister */
+
+typedef struct
+{
+  jerryx_module_link_t link;
+  jerry_value_t result;
+} jerryx_instance_t;
+
+static const char not_found_prologue[] = "Module '";
+static const char not_found_epilogue[] = "' not found";
+
+jerry_value_t
+jerryx_module_load (const jerry_char_t *name)
+{
+  jerry_value_t return_value = 0;
+  size_t error_message_size = 0, name_length = strlen ((char *) name);
+  char *error_message = NULL;
+  jerryx_module_t *module = NULL;
+  jerryx_module_instance_t **instances_pp = JERRYX_MODULE_CONTEXT;
+  jerryx_instance_t *instance_p = NULL;
+
+  if (instances_pp)
+  {
+    instance_p = (jerryx_instance_t *) jerryx_module_link_find (*((jerryx_module_link_t **) instances_pp), name, NULL);
+  }
+
+  if (instance_p)
+  {
+    return instance_p->result;
+  }
+
+  module = (jerryx_module_t *) jerryx_module_link_find ((jerryx_module_link_t *) modules, name, NULL);
+
+  if (module)
+  {
+    return_value = module->init ();
+    if (instances_pp)
+    {
+      instance_p = (jerryx_instance_t *) jmem_heap_alloc_block_null_on_error (sizeof (jerryx_instance_t));
+      if (instance_p)
+      {
+        JERRYX_MODULE_LINK_RUNTIME_INIT (instance_p, module->link.name);
+        instance_p->result = jerry_acquire_value (return_value);
+        jerryx_module_link_insert ((jerryx_module_link_t *) instance_p, (jerryx_module_link_t **) instances_pp);
+      }
+    }
+    return return_value;
+  }
+
+  error_message_size = 1 + name_length + sizeof (not_found_prologue) - 1 + sizeof (not_found_epilogue) - 1;
+  error_message = jmem_heap_alloc_block_null_on_error (error_message_size);
+  if (error_message)
+  {
+    memcpy (error_message, not_found_prologue, sizeof (not_found_prologue) - 1);
+    memcpy (&error_message[sizeof (not_found_prologue) - 1], ((char *) name), name_length);
+    memcpy (&error_message[sizeof (not_found_prologue) - 1 + name_length], not_found_epilogue,
+            sizeof (not_found_epilogue) - 1);
+    error_message[error_message_size - 1] = 0;
+    return_value = jerry_create_error (JERRY_ERROR_COMMON, (jerry_char_t *) error_message);
+    jmem_heap_free_block (error_message, error_message_size);
+  }
+
+  return return_value;
+} /* jerryx_module_load */

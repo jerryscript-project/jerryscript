7cc9d65c095c6631bb547d863dfeadc470d8dfc6
fbmrk <mfabo@inf.u-szeged.hu>
2017-06-16 20:25:00 +0200

    target: mbedos5: use jerry-ext (#1889)
    Update target mbedos5 to use external function handlers: `assert`, `gc` and `print` from jerry-ext.
    
    JerryScript-DCO-1.0-Signed-off-by: Marko Fabo mfabo@inf.u-szeged.hu

--- targets/mbedos5/jerryscript-mbed/jerryscript-mbed-drivers/lib_drivers.h
+++ targets/mbedos5/jerryscript-mbed/jerryscript-mbed-drivers/lib_drivers.h
@@ -15,25 +15,23 @@
 #ifndef _JERRYSCRIPT_MBED_DRIVERS_LIB_DRIVERS_H
 #define _JERRYSCRIPT_MBED_DRIVERS_LIB_DRIVERS_H
 
+#include "jerryscript-ext/handler.h"
 #include "jerryscript-mbed-drivers/InterruptIn-js.h"
 #include "jerryscript-mbed-drivers/DigitalOut-js.h"
 #include "jerryscript-mbed-drivers/setInterval-js.h"
 #include "jerryscript-mbed-drivers/setTimeout-js.h"
-#include "jerryscript-mbed-drivers/print-js.h"
-#include "jerryscript-mbed-drivers/assert-js.h"
 #include "jerryscript-mbed-drivers/I2C-js.h"
-#include "jerryscript-mbed-drivers/gc-js.h"
 #include "jerryscript-mbed-drivers/AnalogIn-js.h"
 #include "jerryscript-mbed-drivers/PwmOut-js.h"
 
 DECLARE_JS_WRAPPER_REGISTRATION (base) {
-    REGISTER_GLOBAL_FUNCTION(assert);
-    REGISTER_GLOBAL_FUNCTION(gc);
+    REGISTER_GLOBAL_FUNCTION_WITH_HANDLER(assert, jerryx_handler_assert);
+    REGISTER_GLOBAL_FUNCTION_WITH_HANDLER(gc, jerryx_handler_gc);
+    REGISTER_GLOBAL_FUNCTION_WITH_HANDLER(print, jerryx_handler_print);
     REGISTER_GLOBAL_FUNCTION(setInterval);
     REGISTER_GLOBAL_FUNCTION(setTimeout);
     REGISTER_GLOBAL_FUNCTION(clearInterval);
     REGISTER_GLOBAL_FUNCTION(clearTimeout);
-    REGISTER_GLOBAL_FUNCTION(print);
     REGISTER_CLASS_CONSTRUCTOR(DigitalOut);
     REGISTER_CLASS_CONSTRUCTOR(I2C);
     REGISTER_CLASS_CONSTRUCTOR(InterruptIn);

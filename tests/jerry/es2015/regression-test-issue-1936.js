52d0c4b243523cf7cd8650b6391488cd22776be9
Zidong Jiang <zidong.jiang@intel.com>
2017-07-27 18:42:15 +0800

    Bugfix: the value in test cases should be invariable
    JerryScript-DCO-1.0-Signed-off-by: Zidong Jiang zidong.jiang@intel.com

--- tests/jerry/es2015/regression-test-issue-1936.js
+++ tests/jerry/es2015/regression-test-issue-1936.js
@@ -17,7 +17,7 @@ var name = "";
 try
 {
   Object.defineProperties(constructor.isExtensible, {a: Object.getOwnPropertyDescriptor(Uint8ClampedArray, "length")})
-  new Int32Array(new ArrayBuffer(), undefined, Date.now())
+  new Int32Array(new ArrayBuffer(), undefined, 40000000000)
 }
 catch (e)
 {

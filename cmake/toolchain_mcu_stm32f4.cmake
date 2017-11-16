36479ddc1a92ff5a8a2acda6b1af8e4644f38b7b
Akos Kiss <akiss@inf.u-szeged.hu>
2017-11-16 16:19:00 +0100

    Remove the 'external' toolchain file (#2089)
    The 'external' toolchain file does nothing but transitively sets
    some cmake system variables from values received on the command
    line, and forcibly sets the C compiler. However, the same cmake
    system variables can be directly set via the command line, together
    with the C compiler, and specifying a toolchain is not a must.
    Thus, this patch drops the superfluous 'external' toolchain file
    and updates cmake-based targets to invoke cmake in a simpler form.
    
    Related changes in this commit:
    - While updating the cmake invocations, all the command line
      arguments have been reviewed and simplified (removed those, which
      did not change the defaults).
    - Removed unnecessary forced C compiler settings from some
      toolchain files (and/or changed them to setting the "compiler
      works" flag to true, thus keeping cmake's compiler identification
      logic but disabling some of its overzealous compiler sanity
      checks).
    
    JerryScript-DCO-1.0-Signed-off-by: Akos Kiss akiss@inf.u-szeged.hu

--- cmake/toolchain_mcu_stm32f4.cmake
+++ cmake/toolchain_mcu_stm32f4.cmake
@@ -12,12 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include(CMakeForceCompiler)
-
 set(CMAKE_SYSTEM_NAME MCU)
 set(CMAKE_SYSTEM_PROCESSOR armv7l)
 set(CMAKE_SYSTEM_VERSION STM32F4)
 
 set(FLAGS_COMMON_ARCH -mlittle-endian -mthumb -mcpu=cortex-m4 -march=armv7e-m -mfpu=fpv4-sp-d16 -mfloat-abi=hard)
 
-CMAKE_FORCE_C_COMPILER(arm-none-eabi-gcc GNU)
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_C_COMPILER_WORKS TRUE)

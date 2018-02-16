bbfae389bcbc02937448339ae40d93e47d3bd061
László Langó <llango.u-szeged@partner.samsung.com>
2018-02-16 15:44:33 +0100

    Eliminate the pylint warnings and update the pylint configuration
    JerryScript-DCO-1.0-Signed-off-by: László Langó llango.u-szeged@partner.samsung.com

--- tools/run-tests.py
+++ tools/run-tests.py
@@ -25,11 +25,8 @@ import settings
 
 OUTPUT_DIR = os.path.join(settings.PROJECT_DIR, 'build', 'tests')
 
-class Options(object):
-    def __init__(self, name, build_args=None, test_args=None):
-        self.name = name
-        self.build_args = build_args or []
-        self.test_args = test_args or []
+Options = collections.namedtuple('Options', ['name', 'build_args', 'test_args'])
+Options.__new__.__defaults__ = ([], [])
 
 def get_binary_path(bin_dir_path):
     return os.path.join(bin_dir_path, 'jerry')
@@ -37,13 +34,17 @@ def get_binary_path(bin_dir_path):
 # Test options for unittests
 JERRY_UNITTESTS_OPTIONS = [
     Options('unittests',
-            ['--unittests', '--jerry-cmdline=off', '--error-messages=on', '--snapshot-save=on', '--snapshot-exec=on', '--vm-exec-stop=on', '--profile=es2015-subset', '--mem-stats=on']),
+            ['--unittests', '--jerry-cmdline=off', '--error-messages=on', '--snapshot-save=on',
+             '--snapshot-exec=on', '--vm-exec-stop=on', '--profile=es2015-subset', '--mem-stats=on']),
     Options('unittests-debug',
-            ['--unittests', '--jerry-cmdline=off', '--debug', '--error-messages=on', '--snapshot-save=on', '--snapshot-exec=on', '--vm-exec-stop=on', '--profile=es2015-subset', '--mem-stats=on']),
+            ['--unittests', '--jerry-cmdline=off', '--debug', '--error-messages=on', '--snapshot-save=on',
+             '--snapshot-exec=on', '--vm-exec-stop=on', '--profile=es2015-subset', '--mem-stats=on']),
     Options('doctests',
-            ['--doctests', '--jerry-cmdline=off', '--error-messages=on', '--snapshot-save=on', '--snapshot-exec=on', '--vm-exec-stop=on', '--profile=es2015-subset']),
+            ['--doctests', '--jerry-cmdline=off', '--error-messages=on', '--snapshot-save=on',
+             '--snapshot-exec=on', '--vm-exec-stop=on', '--profile=es2015-subset']),
     Options('doctests-debug',
-            ['--doctests', '--jerry-cmdline=off', '--debug', '--error-messages=on', '--snapshot-save=on', '--snapshot-exec=on', '--vm-exec-stop=on', '--profile=es2015-subset'])
+            ['--doctests', '--jerry-cmdline=off', '--debug', '--error-messages=on',
+             '--snapshot-save=on', '--snapshot-exec=on', '--vm-exec-stop=on', '--profile=es2015-subset'])
 ]
 
 # Test options for jerry-tests
@@ -84,7 +85,8 @@ JERRY_TEST_SUITE_OPTIONS.extend([
             ['--profile=es2015-subset', '--snapshot-save=on', '--snapshot-exec=on', '--jerry-cmdline-snapshot=on'],
             ['--snapshot']),
     Options('jerry_test_suite-es2015_subset-debug-snapshot',
-            ['--debug', '--profile=es2015-subset', '--snapshot-save=on', '--snapshot-exec=on', '--jerry-cmdline-snapshot=on'],
+            ['--debug', '--profile=es2015-subset', '--snapshot-save=on', '--snapshot-exec=on',
+             '--jerry-cmdline-snapshot=on'],
             ['--snapshot'])
 ])
 
@@ -342,9 +344,9 @@ def run_buildoption_test(options):
 
     return ret
 
-def main(options):
-    Check = collections.namedtuple('Check', ['enabled', 'runner', 'arg'])
+Check = collections.namedtuple('Check', ['enabled', 'runner', 'arg'])
 
+def main(options):
     checks = [
         Check(options.check_signed_off, run_check, [settings.SIGNED_OFF_SCRIPT]
               + {'tolerant': ['--tolerant'], 'travis': ['--travis']}.get(options.check_signed_off, [])),

e1766967d4db93c9b0f222c934870479ff104854
László Langó <llango.u-szeged@partner.samsung.com>
2018-09-20 16:50:53 +0200

    Fix '--skip-list' argument of the test runner script.
    JerryScript-DCO-1.0-Signed-off-by: László Langó llango.u-szeged@partner.samsung.com

--- tools/runners/run-test-suite.sh
+++ tools/runners/run-test-suite.sh
@@ -53,7 +53,7 @@ fi
 if [[ "$1" =~ ^--skip-list=.* ]]
 then
     SKIP_LIST=${1#--skip-list=}
-    SKIP_LIST=(${SKIP_LIST//,/ })
+    SKIP_LIST=${SKIP_LIST//,/ }
     shift
 fi
 
@@ -97,7 +97,7 @@ else
 fi
 
 # Remove the skipped tests from list
-for TEST in "${SKIP_LIST[@]}"
+for TEST in ${SKIP_LIST}
 do
     ( sed -i -r "/$TEST/d" $TEST_FILES )
 done

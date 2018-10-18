aeaa73b3fb0849ff9c07825f9dee96b9e01a5049
Csaba Repasi <repasics@inf.u-szeged.hu>
2018-10-18 14:22:32 +0200

    Enable test cases
    Enabled previously disabled test cases in math-pow.js. Everything works as expected.
    
    JerryScript-DCO-1.0-Signed-off-by: Csaba Repasi repasics@inf.u-szeged.hu

--- tests/jerry/math-pow.js
+++ tests/jerry/math-pow.js
@@ -14,7 +14,7 @@
 
 assert ( isNaN (Math.pow (0.0 /* any number */, NaN)) );
 assert ( Math.pow (NaN, 0.0) === 1.0 );
-// assert ( Math.pow (NaN, -0.0) === 1.0 );
+assert ( Math.pow (NaN, -0.0) === 1.0 );
 assert ( isNaN (Math.pow (NaN, 1.0 /* any non-zero number */)) );
 assert ( Math.pow (2.0, Infinity) === Infinity );
 assert ( Math.pow (2.0, -Infinity) === 0.0 );
@@ -27,17 +27,17 @@ assert ( Math.pow (Infinity, -1.0) === 0 );
 assert ( Math.pow (-Infinity, 3.0) === -Infinity );
 assert ( Math.pow (-Infinity, 2.0) === Infinity );
 assert ( Math.pow (-Infinity, 2.5) === Infinity );
-// assert ( Math.pow (-Infinity, -3.0) === -0.0 );
+assert ( Math.pow (-Infinity, -3.0) === -0.0 );
 assert ( Math.pow (-Infinity, -2.0) === 0.0 );
 assert ( Math.pow (-Infinity, -2.5) === 0.0 );
 assert ( Math.pow (0.0, 1.2) === 0.0 );
 assert ( Math.pow (0.0, -1.2) === Infinity );
-// assert ( Math.pow (-0.0, 3.0) === -0.0 );
-// assert ( Math.pow (-0.0, 2.0) === 0.0 );
-// assert ( Math.pow (-0.0, 2.5) === 0.0 );
-// assert ( Math.pow (-0.0, -3.0) === -Infinity );
-// assert ( Math.pow (-0.0, -2.0) === Infinity );
-// assert ( Math.pow (-0.0, -2.5) === Infinity );
+assert ( Math.pow (-0.0, 3.0) === -0.0 );
+assert ( Math.pow (-0.0, 2.0) === 0.0 );
+assert ( Math.pow (-0.0, 2.5) === 0.0 );
+assert ( Math.pow (-0.0, -3.0) === -Infinity );
+assert ( Math.pow (-0.0, -2.0) === Infinity );
+assert ( Math.pow (-0.0, -2.5) === Infinity );
 assert ( isNaN (Math.pow (-3, 2.5)) );
 
 assert(Math.pow (-2, 2) === 4);

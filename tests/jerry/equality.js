74d474248dfe4612d21a17015f74e5ae244279ac
matedabis <43994185+matedabis@users.noreply.github.com>
2018-10-31 09:53:21 +0100

    Enable the already fixed test cases (#2570)
    Enable already fixed asserts
    
    JerryScript-DCO-1.0-Signed-off-by: Mate Dabis mdabis@inf.u-szeged.hu

--- tests/jerry/equality.js
+++ tests/jerry/equality.js
@@ -16,17 +16,17 @@ assert((5 == 5) == true);
 assert((7 != 2) == true);
 
 var num = 0;
-//var obj = new String("0");
+var obj = new String("0");
 var str = "0";
 var b = false;
 
 assert(num === num);
-//assert(obj === obj);
+assert(obj === obj);
 assert(str === str);
 
-//assert((num === obj) == false);
+assert((num === obj) == false);
 assert((num === str) == false);
-//assert((obj === str) == false);
-//assert((null === undefined) == false);
-//assert((obj === null) == false);
-//assert((obj === undefined) == false);
+assert((obj === str) == false);
+assert((null === undefined) == false);
+assert((obj === null) == false);
+assert((obj === undefined) == false);

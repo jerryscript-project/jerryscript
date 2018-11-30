74df87257245d653a36d5be4d3bc1c7ce5ef7481
wanjochan <wanjochan@gmail.com>
2018-11-30 17:49:09 +0800

    add copyright to jerryscript-libc.h
    Signed-off-by: wanjochan <wanjochan@gmail.com>

--- jerry-core/include/jerryscript-libc.h
+++ jerry-core/include/jerryscript-libc.h
@@ -1,5 +1,20 @@
-#ifndef _JERRY_LIBC_H
-#define _JERRY_LIBC_H
+/* Copyright JS Foundation and other contributors, http://js.foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef JERRYSCRIPT_LIBC_H
+#define JERRYSCRIPT_LIBC_H
 
 #if defined JERRY_USING_LIBDL && JERRY_USING_LIBDL
 
@@ -50,4 +65,4 @@
 #endif /* __GNUC__ */
 
 #endif//JERRY_USING_LIBDL
-#endif//_JERRY_LIBC_H
+#endif//JERRYSCRIPT_LIBC_H

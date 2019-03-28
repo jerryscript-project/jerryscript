59ee59fcd76e4b4ccd2c6343368cf61fec32ba45
László Langó <llango.u-szeged@partner.samsung.com>
2019-03-28 09:24:03 +0100

    Fixed 'jerry_get_object_keys' API function.
    'jerry_get_object_keys' always gave back an empty array.
    
    JerryScript-DCO-1.0-Signed-off-by: László Langó llango.u-szeged@partner.samsung.com

--- tests/unit-core/test-api.c
+++ tests/unit-core/test-api.c
@@ -650,6 +650,7 @@ main (void)
   res = jerry_get_object_keys (global_obj_val);
   TEST_ASSERT (!jerry_value_is_error (res));
   TEST_ASSERT (jerry_value_is_array (res));
+  TEST_ASSERT (jerry_get_array_length (res) == 15);
   jerry_release_value (res);
 
   /* Test: jerry_value_to_primitive */

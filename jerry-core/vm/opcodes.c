9a512c8fcf1882c71f6223472c33d58586b95b07
Dániel Bátyai <dbatyai@inf.u-szeged.hu>
2019-05-16 15:34:41 +0200

    Properly release property name collection in for-in opcode handler (#2875)
    JerryScript-DCO-1.0-Signed-off-by: Dániel Bátyai dbatyai@inf.u-szeged.hu

--- jerry-core/vm/opcodes.c
+++ jerry-core/vm/opcodes.c
@@ -252,7 +252,9 @@ opfunc_for_in (ecma_value_t left_value, /**< left value */
     *result_obj_p = ecma_make_object_value (obj_p);
   }
 
-  jmem_heap_free_block (prop_names_coll_p, sizeof (ecma_collection_header_t));
+  /* Note: We release the collection header here, and return the chunk list of the collection, which will
+   * be handled and released by the vm loop. */
+  jmem_pools_free (prop_names_coll_p, sizeof (ecma_collection_header_t));
   ecma_free_value (obj_expr_value);
 
   return prop_names_p;

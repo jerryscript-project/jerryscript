e085c18e4719b807843233ef2728189c47befb8c
Robert Fancsik <frobert@inf.u-szeged.hu>
2019-09-09 10:58:35 +0200

    Fix String.prototype.repeat for empty strings
    This patch fixes #3084.
    
    JerryScript-DCO-1.0-Signed-off-by: Robert Fancsik frobert@inf.u-szeged.hu

--- jerry-core/ecma/builtin-objects/ecma-builtin-string-prototype.c
+++ jerry-core/ecma/builtin-objects/ecma-builtin-string-prototype.c
@@ -1907,12 +1907,12 @@ ecma_builtin_string_prototype_object_repeat (ecma_string_t *original_string_p, /
     return ecma_raise_range_error (ECMA_ERR_MSG ("Invalid count value"));
   }
 
-  if (length == 0 || isNan)
+  lit_utf8_size_t size = ecma_string_get_utf8_size (original_string_p);
+
+  if (length == 0 || size == 0 || isNan)
   {
     return ecma_make_magic_string_value (LIT_MAGIC_STRING__EMPTY);
   }
-
-  lit_utf8_size_t size = ecma_string_get_utf8_size (original_string_p);
 
   if ((uint32_t) length >= (ECMA_STRING_SIZE_LIMIT / size))
   {

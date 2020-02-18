44e63e038dd9381001d47da5e77e8dcc1656e2b4
Szilagyi Adam <aszilagy@inf.u-szeged.hu>
2020-02-18 09:47:53 +0100

    Use stringbuilder in ecma_get_symbol_descriptive_string (#3560)
    JerryScript-DCO-1.0-Signed-off-by: Adam Szilagyi aszilagy@inf.u-szeged.hu

--- jerry-core/ecma/operations/ecma-symbol-object.c
+++ jerry-core/ecma/operations/ecma-symbol-object.c
@@ -22,6 +22,7 @@
 #include "ecma-objects.h"
 #include "ecma-objects-general.h"
 #include "ecma-symbol-object.h"
+#include "lit-char-helpers.h"
 
 #if ENABLED (JERRY_ES2015)
 
@@ -127,13 +128,14 @@ ecma_get_symbol_descriptive_string (ecma_value_t symbol_value) /**< symbol to st
   ecma_string_t *symbol_p = ecma_get_symbol_from_value (symbol_value);
   ecma_string_t *string_desc_p = ecma_get_symbol_description (symbol_p);
 
+  ecma_stringbuilder_t builder = ecma_stringbuilder_create ();
+
   /* 5. */
-  ecma_string_t *concat_p = ecma_concat_ecma_strings (ecma_get_magic_string (LIT_MAGIC_STRING_SYMBOL_LEFT_PAREN_UL),
-                                                      string_desc_p);
+  ecma_stringbuilder_append_raw (&builder, (lit_utf8_byte_t *) ("Symbol("), 7);
+  ecma_stringbuilder_append (&builder, string_desc_p);
+  ecma_stringbuilder_append_byte (&builder, LIT_CHAR_RIGHT_PAREN);
 
-  ecma_string_t *final_str_p = ecma_append_magic_string_to_string (concat_p, LIT_MAGIC_STRING_RIGHT_PAREN);
-
-  return ecma_make_string_value (final_str_p);
+  return ecma_make_string_value (ecma_stringbuilder_finalize (&builder));
 } /* ecma_get_symbol_descriptive_string */
 
 /**

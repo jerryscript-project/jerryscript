7582590b175f3d1a70409ad036b452a20c94f597
Dániel Bátyai <dbatyai@inf.u-szeged.hu>
2020-06-04 17:13:35 +0200

    Primitive this values of accessors should not be coerced in strict mode
    JerryScript-DCO-1.0-Signed-off-by: Dániel Bátyai dbatyai@inf.u-szeged.hu

--- tests/jerry/getter-setter-this-value.js
+++ tests/jerry/getter-setter-this-value.js
@@ -32,3 +32,19 @@ Object.defineProperty(Box.prototype, 'data', {
 assert(box.data === '=');
 box.data = '+';
 assert(box.data === '+');
+
+function test_access(value, proto) {
+  "use strict"
+
+  Object.defineProperty(proto, 'test', {
+    get: function () { assert (this === value) },
+    set: function () { assert (this === value) }
+  });
+
+  value.test;
+  value.test = undefined;
+}
+
+test_access ("str", String.prototype);
+test_access (1, Number.prototype);
+test_access (true, Boolean.prototype);

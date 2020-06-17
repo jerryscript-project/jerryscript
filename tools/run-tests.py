c55a0baaf89bf347b07d8397fb080161108d941e
Csaba Osztrogonác <oszi@inf.u-szeged.hu>
2020-06-17 15:42:51 +0200

    Improve test262 ES2015 runner (#3892)
    Changes:
     - Add options to the test runner to be able to update exclude list
     - Update the exclude list
     - Make Travis CI signal if the exclude list isn't up-to-date
    
    JerryScript-DCO-1.0-Signed-off-by: Csaba Osztrogonác oszi@inf.u-szeged.hu

--- tools/run-tests.py
+++ tools/run-tests.py
@@ -194,8 +194,10 @@ def get_arguments():
                         help='Run jerry-tests')
     parser.add_argument('--test262', action='store_true',
                         help='Run test262 - ES5.1')
-    parser.add_argument('--test262-es2015', action='store_true',
-                        help='Run test262 - ES2015')
+    parser.add_argument('--test262-es2015', default=False, const='default',
+                        nargs='?', choices=['default', 'all', 'update'],
+                        help='Run test262 - ES2015. default: all tests except excludelist, ' +
+                        'all: all tests, update: all tests and update excludelist')
     parser.add_argument('--unittests', action='store_true',
                         help='Run unittests (including doctests)')
     parser.add_argument('--buildoption-test', action='store_true',
@@ -412,6 +414,7 @@ def run_test262_test_suite(options):
 
         if '--profile=es.next' in job.build_args:
             test_cmd.append('--es2015')
+            test_cmd.append(options.test262_es2015)
         else:
             test_cmd.append('--es51')
 

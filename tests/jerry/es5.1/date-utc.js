e227634b45e05c97d43ac4fc3e34d187b0953777
Csaba Osztrogonác <csaba.osztrogonac@h-lab.eu>
2020-10-01 16:34:09 +0200

    Make Date.UTC to conform to the latest ES11 spec (#4250)
    Date.UTC should work with only one argument too.
    
    JerryScript-DCO-1.0-Signed-off-by: Csaba Osztrogonác csaba.osztrogonac@h-lab.eu

--- tests/jerry/es5.1/date-utc.js
+++ tests/jerry/es5.1/date-utc.js
@@ -12,34 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var d;
-
-d = Date.UTC(undefined);
+var d = Date.UTC(2015);
 assert (isNaN(d));
-
-d = Date.UTC({});
-assert (isNaN(d));
-
-d = Date.UTC(2000 + 15, 0);
-assert (d == 1420070400000);
-
-d = Date.UTC(2015, 0);
-assert (d == 1420070400000);
-
-d = Date.UTC(2015, 0, 1);
-assert (d == 1420070400000);
-
-d = Date.UTC(2015, 0, 1, 0);
-assert (d == 1420070400000);
-
-d = Date.UTC(2015, 0, 1, 0, 0);
-assert (d == 1420070400000);
-
-d = Date.UTC(2015, 0, 1, 0, 0, 0);
-assert (d == 1420070400000);
-
-d = Date.UTC(2015, 0, 1, 0, 0, 0, 0);
-assert (d == 1420070400000);
-
-d = Date.UTC(2015, 6, 3, 14, 35, 43, 123);
-assert (d == 1435934143123);

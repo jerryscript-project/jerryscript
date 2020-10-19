3eea728dd23f8a7350cc027a34f152ab439d15d2
Roland Takacs <roland.takacs@h-lab.eu>
2020-10-19 13:20:08 +0200

    Initialize variable to eliminate `may be used uninitialized` warnings
    JerryScript-DCO-1.0-Signed-off-by: Roland Takacs roland.takacs@h-lab.eu

--- jerry-core/ecma/builtin-objects/typedarray/ecma-builtin-typedarray-prototype.c
+++ jerry-core/ecma/builtin-objects/typedarray/ecma-builtin-typedarray-prototype.c
@@ -1745,7 +1745,7 @@ ecma_builtin_typedarray_prototype_dispatch_routine (uint16_t builtin_routine_id,
   }
 
   ecma_object_t *typedarray_p = ecma_get_object_from_value (this_arg);
-  ecma_typedarray_info_t info;
+  ecma_typedarray_info_t info = { 0 };
 
   if (builtin_routine_id < ECMA_TYPEDARRAY_PROTOTYPE_ROUTINE_BUFFER_GETTER)
   {

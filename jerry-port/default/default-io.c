81702ff5eaf2c047ee00559be3205fae3eaa2c76
Akos Kiss <akiss@inf.u-szeged.hu>
2020-11-16 11:54:01 +0100

    Drop the minimal variant of the default port implementation (#4331)
    The minimal variant became quite meaningless lately. There were two
    port APIs originally that had extra functions in the default port
    in addition to the core-mandated implementations: the I/O and
    Termination port APIs. However, the extra Termination API code was
    removed a year ago, leaving some minimal extension in the I/O port
    only. As the overhead of the extension is negligible, it is not
    worth maintaining two library variants.
    
    Therefore
    - this commit removes the minimal variant of the default port lib,
    - rewrites uses of the minimal variant to use the variant with the
      I/O extension, and
    - updates targets where I/O port code was copy-n-pasted.
    
    JerryScript-DCO-1.0-Signed-off-by: Akos Kiss akiss@inf.u-szeged.hu

--- jerry-port/default/default-io.c
+++ jerry-port/default/default-io.c
@@ -21,23 +21,15 @@
 #include "jerryscript-port-default.h"
 #include "jerryscript-debugger.h"
 
-#ifndef DISABLE_EXTRA_API
-
 /**
  * Actual log level
  */
 static jerry_log_level_t jerry_port_default_log_level = JERRY_LOG_LEVEL_ERROR;
 
-#define JERRY_PORT_DEFAULT_LOG_LEVEL jerry_port_default_log_level
-
 /**
  * Get the log level
  *
  * @return current log level
- *
- * Note:
- *      This function is only available if the port implementation library is
- *      compiled without the DISABLE_EXTRA_API macro.
  */
 jerry_log_level_t
 jerry_port_default_get_log_level (void)
@@ -47,20 +39,12 @@ jerry_port_default_get_log_level (void)
 
 /**
  * Set the log level
- *
- * Note:
- *      This function is only available if the port implementation library is
- *      compiled without the DISABLE_EXTRA_API macro.
  */
 void
 jerry_port_default_set_log_level (jerry_log_level_t level) /**< log level */
 {
   jerry_port_default_log_level = level;
 } /* jerry_port_default_set_log_level */
-
-#else /* DISABLE_EXTRA_API */
-#define JERRY_PORT_DEFAULT_LOG_LEVEL JERRY_LOG_LEVEL_ERROR
-#endif /* !DISABLE_EXTRA_API */
 
 /**
  * Default implementation of jerry_port_log. Prints log message to the standard
@@ -69,18 +53,13 @@ jerry_port_default_set_log_level (jerry_log_level_t level) /**< log level */
  *
  * If debugger support is enabled, printing happens first to an in-memory buffer,
  * which is then sent both to the standard error and to the debugger client.
- *
- * Note:
- *      Changing the log level from JERRY_LOG_LEVEL_ERROR is only possible if
- *      the port implementation library is compiled without the
- *      DISABLE_EXTRA_API macro.
  */
 void
 jerry_port_log (jerry_log_level_t level, /**< message log level */
                 const char *format, /**< format string */
                 ...)  /**< parameters */
 {
-  if (level <= JERRY_PORT_DEFAULT_LOG_LEVEL)
+  if (level <= jerry_port_default_log_level)
   {
     va_list args;
     va_start (args, format);
--- targets/esp-idf/io.c
+++ targets/esp-idf/io.c
@@ -25,34 +25,26 @@ static const char TAG[] = "JS";
 
 static esp_log_level_t crosslog(jerry_log_level_t level)
 {
-  switch(level) 
+  switch(level)
   {
     case JERRY_LOG_LEVEL_ERROR: return ESP_LOG_ERROR;
     case JERRY_LOG_LEVEL_WARNING: return ESP_LOG_WARN;
     case JERRY_LOG_LEVEL_DEBUG: return ESP_LOG_DEBUG;
     case JERRY_LOG_LEVEL_TRACE: return ESP_LOG_VERBOSE;
   }
-    
+
  return ESP_LOG_NONE;
 }
-
-#ifndef DISABLE_EXTRA_API
 
 /**
  * Actual log level
  */
 static jerry_log_level_t jerry_port_default_log_level = JERRY_LOG_LEVEL_ERROR;
 
-#define JERRY_PORT_DEFAULT_LOG_LEVEL jerry_port_default_log_level
-
 /**
  * Get the log level
  *
  * @return current log level
- *
- * Note:
- *      This function is only available if the port implementation library is
- *      compiled without the DISABLE_EXTRA_API macro.
  */
 jerry_log_level_t
 jerry_port_default_get_log_level (void)
@@ -62,20 +54,12 @@ jerry_port_default_get_log_level (void)
 
 /**
  * Set the log level
- *
- * Note:
- *      This function is only available if the port implementation library is
- *      compiled without the DISABLE_EXTRA_API macro.
  */
 void
 jerry_port_default_set_log_level (jerry_log_level_t level) /**< log level */
 {
   jerry_port_default_log_level = level;
 } /* jerry_port_default_set_log_level */
-
-#else /* DISABLE_EXTRA_API */
-#define JERRY_PORT_DEFAULT_LOG_LEVEL JERRY_LOG_LEVEL_ERROR
-#endif /* !DISABLE_EXTRA_API */
 
 /**
  * Default implementation of jerry_port_log. Prints log message to the standard
@@ -84,18 +68,13 @@ jerry_port_default_set_log_level (jerry_log_level_t level) /**< log level */
  *
  * If debugger support is enabled, printing happens first to an in-memory buffer,
  * which is then sent both to the standard error and to the debugger client.
- *
- * Note:
- *      Changing the log level from JERRY_LOG_LEVEL_ERROR is only possible if
- *      the port implementation library is compiled without the
- *      DISABLE_EXTRA_API macro.
  */
 void
 jerry_port_log (jerry_log_level_t level, /**< message log level */
                 const char *format, /**< format string */
                 ...)  /**< parameters */
 {
-  if (level <= JERRY_PORT_DEFAULT_LOG_LEVEL)
+  if (level <= jerry_port_default_log_level)
   {
     va_list args;
     va_start (args, format);

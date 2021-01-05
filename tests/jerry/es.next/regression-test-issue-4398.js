63341629ec02ed5766918e3c505e50b47d62bf68
Robert Fancsik <frobert@inf.u-szeged.hu>
2021-01-05 20:17:44 +0100

    Proxy.[[Set]] should reject fallish trap result in strict mode
    This patch fixes #4398.
    
    JerryScript-DCO-1.0-Signed-off-by: Robert Fancsik frobert@inf.u-szeged.hu

---
+++ tests/jerry/es.next/regression-test-issue-4398.js
@@ -0,0 +1,25 @@
+// Copyright JS Foundation and other contributors, http://js.foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var get = [];
+var constructor = Function();
+constructor[Symbol.species] = Object;
+var p = new Proxy({ constructor: constructor, flags: '', exec: function() { return null; } }, { set: function (x) { this.set = x === 42; } });
+
+try {
+  RegExp.prototype[Symbol.split].call(p, 7996);
+  assert(false);
+} catch (e) {
+  assert(e instanceof TypeError);
+}

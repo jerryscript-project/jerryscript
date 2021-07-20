7de391931a3a7a67e3db93faf84d5b4df311793f
Virag Orkenyi <orkvi@inf.u-szeged.hu>
2021-07-20 11:28:30 +0200

    Add custom dispatcher to set-iterator-prototype (#4717)
    JerryScript-DCO-1.0-Signed-off-by: Orkenyi Virag orkvi@inf.u-szeged.hu

--- jerry-core/ecma/builtin-objects/ecma-builtin-set-iterator-prototype.c
+++ jerry-core/ecma/builtin-objects/ecma-builtin-set-iterator-prototype.c
@@ -22,6 +22,20 @@
 #define ECMA_BUILTINS_INTERNAL
 #include "ecma-builtins-internal.h"
 
+/**
+ * This object has a custom dispatch function.
+ */
+#define BUILTIN_CUSTOM_DISPATCH
+
+/**
+ * List of built-in routine identifiers.
+ */
+enum
+{
+  ECMA_BUILTIN_SET_ITERATOR_PROTOTYPE_ROUTINE_START = 0,
+  ECMA_BUILTIN_SET_ITERATOR_PROTOTYPE_OBJECT_NEXT,
+};
+
 #define BUILTIN_INC_HEADER_NAME "ecma-builtin-set-iterator-prototype.inc.h"
 #define BUILTIN_UNDERSCORED_ID set_iterator_prototype
 #include "ecma-builtin-internal-routines-template.inc.h"
@@ -53,6 +67,36 @@ ecma_builtin_set_iterator_prototype_object_next (ecma_value_t this_val) /**< thi
 {
   return ecma_op_container_iterator_next (this_val, ECMA_OBJECT_CLASS_SET_ITERATOR);
 } /* ecma_builtin_set_iterator_prototype_object_next */
+
+/**
+ * Dispatcher of the built-in's routines
+ *
+ * @return ecma value
+ *         Returned value must be freed with ecma_free_value.
+ */
+ecma_value_t
+ecma_builtin_set_iterator_prototype_dispatch_routine (uint8_t builtin_routine_id, /**< built-in wide
+                                                                                   *routine identifier */
+                                                      ecma_value_t this_arg, /**< 'this' argument value */
+                                                      const ecma_value_t arguments_list[], /**< list of arguments
+                                                                                            *   passed to routine */
+                                                      uint32_t arguments_number) /**< length of arguments' list */
+{
+  JERRY_UNUSED (arguments_list);
+  JERRY_UNUSED (arguments_number);
+
+  switch (builtin_routine_id)
+  {
+    case ECMA_BUILTIN_SET_ITERATOR_PROTOTYPE_OBJECT_NEXT:
+    {
+      return ecma_builtin_set_iterator_prototype_object_next (this_arg);
+    }
+    default:
+    {
+      JERRY_UNREACHABLE ();
+    }
+  }
+} /* ecma_builtin_set_iterator_prototype_dispatch_routine */
 
 /**
  * @}
